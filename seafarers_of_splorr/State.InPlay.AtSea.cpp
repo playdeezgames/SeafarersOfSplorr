7a2dfa57e8e6a5ef0f5ef08c894824de69fe355e
TheGrumpyGameDev <ernestpazera@msn.com>
2021-07-03 04:57:52 -0500

    i can navigate the order menu in the at sea screen

--- seafarers_of_splorr/State.InPlay.AtSea.cpp
+++ seafarers_of_splorr/State.InPlay.AtSea.cpp
@@ -6,6 +6,8 @@
 #include "Game.Avatar.h"
 #include "Visuals.Texts.h"
 #include <format>
+#include "Visuals.Menus.h"
+#include "Common.Utility.h"
 namespace state::in_play::AtSea 
 { 
 	const std::string LAYOUT_NAME = "State.InPlay.AtSea";
@@ -13,9 +15,31 @@ namespace state::in_play::AtSea
 	const std::string TEXT_AVATAR_Y = "avatar-y";
 	const std::string TEXT_AVATAR_HEADING = "avatar-heading";
 	const std::string TEXT_AVATAR_SPEED = "avatar-speed";
+	const std::string MENU_ID = "Order";
+
+	enum class OrderMenuItem
+	{
+		CHANGE_HEADING,
+		CHANGE_SPEED,
+		MOVE
+	};
+
+	const std::map<OrderMenuItem, std::function<void()>> activators =
+	{
+	};
+
+	static void ActivateItem()
+	{
+		common::Utility::Dispatch(activators, (OrderMenuItem)visuals::Menus::ReadIndex(LAYOUT_NAME, MENU_ID).value());
+	}
+
 
 	const std::map<::Command, std::function<void()>> commandHandlers =
 	{
+		{::Command::UP, visuals::Menus::NavigatePrevious(LAYOUT_NAME, MENU_ID) },
+		{::Command::DOWN, visuals::Menus::NavigateNext(LAYOUT_NAME, MENU_ID) },
+		{::Command::GREEN, ActivateItem },
+		{::Command::BACK, ::application::UIState::GoTo(::UIState::LEAVE_PLAY) },
 		{::Command::RED, ::application::UIState::GoTo(::UIState::LEAVE_PLAY) }
 	};
 
