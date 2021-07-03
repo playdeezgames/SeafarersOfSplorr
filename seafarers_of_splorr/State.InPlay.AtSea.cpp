a429dd21e3b2782d93396272d86d23cf3cea72db
TheGrumpyGameDev <ernestpazera@msn.com>
2021-07-03 05:04:59 -0500

    at sea menu done the crude freebooter way

--- seafarers_of_splorr/State.InPlay.AtSea.cpp
+++ seafarers_of_splorr/State.InPlay.AtSea.cpp
@@ -8,6 +8,8 @@
 #include <format>
 #include "Visuals.Menus.h"
 #include "Common.Utility.h"
+#include "Visuals.Areas.h"
+#include "Application.MouseMotion.h"
 namespace state::in_play::AtSea 
 { 
 	const std::string LAYOUT_NAME = "State.InPlay.AtSea";
@@ -16,6 +18,9 @@ namespace state::in_play::AtSea
 	const std::string TEXT_AVATAR_HEADING = "avatar-heading";
 	const std::string TEXT_AVATAR_SPEED = "avatar-speed";
 	const std::string MENU_ID = "Order";
+	const std::string AREA_CHANGE_HEADING = "ChangeHeading";
+	const std::string AREA_CHANGE_SPEED = "ChangeSpeed";
+	const std::string AREA_MOVE = "Move";
 
 	enum class OrderMenuItem
 	{
@@ -55,11 +60,28 @@ namespace state::in_play::AtSea
 		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_SPEED, std::format("Speed: {}", speed));
 	}
 
+	const std::map<std::string, OrderMenuItem> areaMenuItems =
+	{
+		{ AREA_CHANGE_HEADING,  OrderMenuItem::CHANGE_HEADING},
+		{ AREA_CHANGE_SPEED,  OrderMenuItem::CHANGE_SPEED},
+		{ AREA_MOVE,  OrderMenuItem::MOVE}
+	};
+
+	static void SetCurrentMenuItem(OrderMenuItem item)
+	{
+		visuals::Menus::WriteIndex(LAYOUT_NAME, MENU_ID, (int)item);
+	}
+
+	static void OnMouseMotionInArea(const std::string& area, const common::XY<int>&)
+	{
+		SetCurrentMenuItem(areaMenuItems.find(area)->second);
+	}
 
 	void Start()
 	{
 		::application::OnEnter::AddHandler(::UIState::IN_PLAY_AT_SEA, OnEnter);
 		::application::Renderer::SetRenderLayout(::UIState::IN_PLAY_AT_SEA, LAYOUT_NAME);
 		::application::Command::SetHandlers(::UIState::IN_PLAY_AT_SEA, commandHandlers);
+		::application::MouseMotion::AddHandler(::UIState::IN_PLAY_AT_SEA, visuals::Areas::HandleMouseMotion(LAYOUT_NAME, OnMouseMotionInArea));
 	}
 }
