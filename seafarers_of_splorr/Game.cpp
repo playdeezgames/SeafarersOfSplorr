0aaf02c8dd984b6dafd4f765ac0acd0700ce821a
TheGrumpyGameDev <ernestpazera@msn.com>
2021-07-05 17:12:19 -0500

    saving games

--- seafarers_of_splorr/Game.cpp
+++ seafarers_of_splorr/Game.cpp
@@ -4,6 +4,7 @@
 #include "Game.Avatar.h"
 #include "Game.Islands.h"
 #include "Game.World.h"
+#include "Data.Game.World.h"
 namespace game
 {
 	void Reset(const Difficulty& difficulty)
@@ -29,24 +30,34 @@ namespace game
 		data::sqlite::Stores::Copy(data::sqlite::Store::AUTOSAVE, data::sqlite::Store::IN_MEMORY);
 	}
 
-	bool DoesAutosaveExist()
+	static bool DoesWorldExistInStore(const data::sqlite::Store& store)
 	{
-		return false;//TODO: figure this out!
+		return !data::sqlite::Stores::Execute(store, "SELECT name FROM sqlite_master WHERE type='table' AND name='Worlds';").empty();
 	}
 
-	bool DoesSlotExist(int slot)
+	bool DoesAutosaveExist()
 	{
-		return false;//TODO: figure this out!
+		return DoesWorldExistInStore(data::sqlite::Store::AUTOSAVE);
 	}
 
 	const std::map<int, data::sqlite::Store> slotTable =
+	{
+		{1, data::sqlite::Store::SLOT_1},
+		{2, data::sqlite::Store::SLOT_2},
+		{3, data::sqlite::Store::SLOT_3},
+		{4, data::sqlite::Store::SLOT_4},
+		{5, data::sqlite::Store::SLOT_5}
+	};
+
+	bool DoesSlotExist(int slot)
+	{
+		auto iter = slotTable.find(slot);
+		if (iter != slotTable.end())
 		{
-			{0, data::sqlite::Store::SLOT_1},
-			{1, data::sqlite::Store::SLOT_2},
-			{2, data::sqlite::Store::SLOT_3},
-			{3, data::sqlite::Store::SLOT_4},
-			{4, data::sqlite::Store::SLOT_5}
-		};
+			return DoesWorldExistInStore(iter->second);
+		}
+		return false;
+	}
 
 	void LoadFromSlot(int slot)
 	{
