1cec3764f9c628b5c2c4297aafbe445e0a413674
ernestpazera <ernestpazera@gmail.com>
2021-07-23 05:52:42 -0500

    setting this down for now to do the jam, next time pick up ship statistics

--- seafarers_of_splorr/Game.ShipDescriptor.h
+++ seafarers_of_splorr/Game.ShipDescriptor.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <string>
+#include "Game.Commodity.h"
+#include <map>
 namespace game
 {
 	struct ShipDescriptor
 	{
 		std::string name;
+		double speedFactor;//TODO: move this to ship statistics
+		double tonnage;//TODO: move this to ship statistics
+		std::map<game::Commodity, double> commodities;//this is what it takes to build one of these ships
 	};
 }
