81fed7d2f2718cb0c8a2952dc0ad7ce19d78de00
ernestpazera <ernestpazera@gmail.com>
2021-07-26 05:16:18 -0500

    ship statistics exist

--- seafarers_of_splorr/Game.ShipDescriptor.h
+++ seafarers_of_splorr/Game.ShipDescriptor.h
@@ -2,13 +2,13 @@
 #include <string>
 #include "Game.Commodity.h"
 #include <map>
+#include "Game.Ship.Statistic.h"
 namespace game
 {
 	struct ShipDescriptor
 	{
 		std::string name;
-		double speedFactor;//TODO: move this to ship statistics
-		double tonnage;//TODO: move this to ship statistics
+		std::map<game::ship::Statistic, double> statistics;
 		std::map<game::Commodity, double> commodities;//this is what it takes to build one of these ships
 	};
 }
--- seafarers_of_splorr/Game.Ships.cpp
+++ seafarers_of_splorr/Game.Ships.cpp
@@ -4,8 +4,34 @@ namespace game::Ships
 {
 	const std::map<game::Ship, game::ShipDescriptor> ships =
 	{
-		{game::Ship::RAFT, {"raft"}},
-		{game::Ship::JUNK, {"junk"}}
+		{
+			game::Ship::RAFT, 
+			{
+				"raft",
+				{
+					{game::ship::Statistic::SPEED_FACTOR, 0.75},
+					{game::ship::Statistic::TONNAGE, 50.0}
+				},
+				{
+					{game::Commodity::WOOD, 100.0},
+					{game::Commodity::LABOR, 50.0}
+				}
+			}
+		},
+		{
+			game::Ship::JUNK, 
+			{
+				"junk",
+				{
+					{game::ship::Statistic::SPEED_FACTOR, 1.0},
+					{game::ship::Statistic::TONNAGE, 100.0}
+				},
+				{
+					{game::Commodity::WOOD, 200.0},
+					{game::Commodity::LABOR, 75.0}
+				}
+			}
+		}
 	};
 
 	static std::list<game::Ship> shipList;
