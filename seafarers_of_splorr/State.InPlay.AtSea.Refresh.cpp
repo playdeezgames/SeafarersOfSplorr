0b48b169544dbcb3bf1a14adb9ca8fab754ca0f6
ernestpazera <ernestpazera@gmail.com>
2021-07-29 03:38:44 -0500

    moved out at sea refresh functions into their own cpp

---
+++ seafarers_of_splorr/State.InPlay.AtSea.Refresh.cpp
@@ -0,0 +1,192 @@
+#include <format>
+#include "Game.Avatar.h"
+#include "Game.Avatar.Destination.h"
+#include "Game.Avatar.Quest.h"
+#include "Game.Avatar.Statistics.h"
+#include "Game.Heading.h"
+#include "Game.Islands.h"
+#include "Game.World.h"
+#include <map>
+#include <string>
+#include "Visuals.Images.h"
+#include "Visuals.Menus.h"
+#include "Visuals.Texts.h"
+namespace state::in_play::AtSea
+{
+	const std::string LAYOUT_NAME;
+
+	const std::string TEXT_AVATAR_TURNS = "avatar-turns";
+	const std::string TEXT_AVATAR_HEALTH = "avatar-health";
+	const std::string TEXT_AVATAR_HUNGER = "avatar-hunger";
+	const std::string TEXT_AVATAR_HEADING = "avatar-heading";
+	const std::string TEXT_AVATAR_SPEED = "avatar-speed";
+	const std::string TEXT_AVATAR_MONEY = "avatar-money";
+	const std::string TEXT_AVATAR_REPUTATION = "avatar-reputation";
+
+	const std::string FORMAT_TURNS = "Turns Left: {:.0f}";
+	const std::string FORMAT_MONEY = "Money: {:.0f}";
+	const std::string FORMAT_REPUTATION = "Reputation: {:.0f}";
+	const std::string FORMAT_HUNGER = "Hunger: {:.0f}";
+	const std::string FORMAT_HEALTH = "Health: {:.0f}";
+	const std::string FORMAT_HEADING = "Heading: {:.2f}";
+	const std::string FORMAT_SPEED = "Speed: {:.2f}";
+
+	const std::string IMAGE_CURRENT_HEADING = "CurrentHeading";
+	const std::string IMAGE_DESTINATION_1 = "Destination1";
+	const std::string IMAGE_DESTINATION_2 = "Destination2";
+	const std::string IMAGE_DESTINATION_3 = "Destination3";
+	const std::string IMAGE_DESTINATION_4 = "Destination4";
+	const std::string IMAGE_QUEST_DESTINATION = "QuestDestination";
+
+	const std::string MENU_ITEM_DOCK = "Dock";
+	const std::string MENU_ITEM_JOB = "Job";
+
+	static void RefreshAvatarHunger()
+	{
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_HUNGER, std::format(FORMAT_HUNGER, game::avatar::Statistics::GetCurrent(game::avatar::Statistic::HUNGER)));
+	}
+
+	static void RefreshAvatarMoney()
+	{
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_MONEY, std::format(FORMAT_MONEY, game::avatar::Statistics::GetCurrent(game::avatar::Statistic::MONEY)));
+	}
+
+	static void RefreshAvatarReputation()
+	{
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_REPUTATION, std::format(FORMAT_REPUTATION, game::avatar::Statistics::GetCurrent(game::avatar::Statistic::REPUTATION)));
+	}
+
+	static void RefreshAvatarHealth()
+	{
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_HEALTH, std::format(FORMAT_HEALTH, game::avatar::Statistics::GetCurrent(game::avatar::Statistic::HEALTH)));
+	}
+
+	static void RefreshAvatarTurns()
+	{
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_TURNS, std::format(FORMAT_TURNS, game::avatar::Statistics::GetCurrent(game::avatar::Statistic::TURNS_REMAINING)));
+	}
+
+	static void RefreshAvatarHeading()
+	{
+		auto heading = game::Avatar::GetHeading();
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_HEADING, std::format(FORMAT_HEADING, heading));
+		visuals::Images::SetAngle(LAYOUT_NAME, IMAGE_CURRENT_HEADING, heading);
+	}
+
+	static void RefreshAvatarSpeed()
+	{
+		auto speed = game::Avatar::GetSpeed();
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_SPEED, std::format(FORMAT_SPEED, speed));
+	}
+
+	const std::map<game::avatar::Destination, std::string> destinationIdImages =
+	{
+		{game::avatar::Destination::ONE, IMAGE_DESTINATION_1},
+		{game::avatar::Destination::TWO, IMAGE_DESTINATION_2},
+		{game::avatar::Destination::THREE, IMAGE_DESTINATION_3},
+		{game::avatar::Destination::FOUR, IMAGE_DESTINATION_4}
+	};
+
+	static common::XY<double> Plot(const common::XY<double>&);
+	static void RefreshAvatarDestination(const game::avatar::Destination& destinationId)
+	{
+		auto destination = game::avatar::destination::GetDestination(destinationId);
+		auto imageId = destinationIdImages.find(destinationId)->second;
+		if (destination)
+		{
+			auto clampedDistance = game::Heading::ClampDistance(destination.value() - game::Avatar::GetLocation(), game::World::GetViewDistance() + 0.5);//TODO: magic number
+			auto plot = Plot(clampedDistance);
+			visuals::Images::SetLocation(LAYOUT_NAME, imageId, { (int)plot.GetX(), (int)plot.GetY() });
+			visuals::Images::SetVisible(LAYOUT_NAME, imageId, true);
+			return;
+		}
+		visuals::Images::SetVisible(LAYOUT_NAME, imageId, false);
+	}
+
+	static void RefreshAvatarDestinations()
+	{
+		for (auto destinationId : game::avatar::destination::All())
+		{
+			RefreshAvatarDestination(destinationId);
+		}
+	}
+
+	static void RefreshAvatarQuestDestination()
+	{
+		auto quest = game::avatar::Quest::Read();
+		if (quest)
+		{
+			auto clampedDistance = game::Heading::ClampDistance(quest.value().destination - game::Avatar::GetLocation(), game::World::GetViewDistance() + 0.5);//TODO: magic number
+			auto plot = Plot(clampedDistance);
+			visuals::Images::SetLocation(LAYOUT_NAME, IMAGE_QUEST_DESTINATION, { (int)plot.GetX(), (int)plot.GetY() });
+			visuals::Images::SetVisible(LAYOUT_NAME, IMAGE_QUEST_DESTINATION, true);
+			return;
+		}
+		visuals::Images::SetVisible(LAYOUT_NAME, IMAGE_QUEST_DESTINATION, false);
+	}
+
+	//TODO: get this not hardcoded
+	const common::XY<double> VIEW_CENTER = { 162.0,182.0 };
+	const double VIEW_RADIUS = 144.0;
+	const int ISLAND_ICON_COUNT = 10;
+	const int IMAGE_OFFSET_X = 0;
+	const int IMAGE_OFFSET_Y = 0;
+	const int TEXT_OFFSET_X = 0;
+	const int TEXT_OFFSET_Y = 8;
+
+	static void HideVisibleIslands()
+	{
+		for (int icon = 0; icon < ISLAND_ICON_COUNT; ++icon)
+		{
+			auto visualId = std::format("AtSeaIsland{}", icon);
+			visuals::Images::SetVisible(LAYOUT_NAME, visualId, false);
+			visuals::Texts::SetText(LAYOUT_NAME, visualId, "");
+		}
+	}
+
+	static common::XY<double> Plot(const common::XY<double>& location)
+	{
+		double viewScale = VIEW_RADIUS / game::World::GetViewDistance();
+		return location * viewScale + VIEW_CENTER;
+	}
+
+
+	void RefreshAvatarStatus()
+	{
+		RefreshAvatarHeading();
+		RefreshAvatarHealth();
+		RefreshAvatarHunger();
+		RefreshAvatarSpeed();
+		RefreshAvatarMoney();
+		RefreshAvatarReputation();
+		RefreshAvatarTurns();
+		RefreshAvatarDestinations();
+		RefreshAvatarQuestDestination();
+		visuals::MenuItems::SetEnabled(LAYOUT_NAME, MENU_ITEM_JOB, game::avatar::Quest::Read().has_value());
+	}
+
+	void UpdateAutoMoveState(bool);//TODO: this is an update, not a refresh
+
+	void RefreshIslands()
+	{
+		HideVisibleIslands();
+		auto islands = game::Islands::GetViewableIslands();
+		int icon = 0;
+		double dockDistance = game::World::GetDockDistance();
+		bool canDock = false;
+		for (auto& entry : islands)
+		{
+			canDock |= (game::Heading::Distance(entry.relativeLocation, { 0.0, 0.0 }) <= dockDistance);
+			auto plot = Plot(entry.relativeLocation);
+			auto visualId = std::format("AtSeaIsland{}", icon);
+			visuals::Images::SetLocation(LAYOUT_NAME, visualId, { (int)plot.GetX() + IMAGE_OFFSET_X,(int)plot.GetY() + IMAGE_OFFSET_Y });
+			visuals::Images::SetVisible(LAYOUT_NAME, visualId, true);
+			visuals::Texts::SetLocation(LAYOUT_NAME, visualId, { (int)plot.GetX() + TEXT_OFFSET_X,(int)plot.GetY() + TEXT_OFFSET_Y });
+			visuals::Texts::SetText(LAYOUT_NAME, visualId, (entry.visits.has_value()) ? (entry.name) : ("????"));
+			++icon;
+		}
+		visuals::MenuItems::SetEnabled(LAYOUT_NAME, MENU_ITEM_DOCK, canDock);
+		UpdateAutoMoveState(canDock);
+	}
+
+}
--- seafarers_of_splorr/State.InPlay.AtSea.cpp
+++ seafarers_of_splorr/State.InPlay.AtSea.cpp
@@ -5,56 +5,28 @@
 #include "Application.Renderer.h"
 #include "Application.Update.h"
 #include "Common.Utility.h"
-#include <format>
 #include "Game.Audio.Mux.h"
 #include "Game.Avatar.h"
-#include "Game.Avatar.Destination.h"
-#include "Game.Avatar.Quest.h"
-#include "Game.Avatar.Statistics.h"
 #include "Game.Heading.h"
-#include "Game.Islands.h"
-#include "Game.World.h"
-#include "Visuals.Menus.h"
-#include "Visuals.Texts.h"
 #include "Visuals.Areas.h"
 #include "Visuals.Images.h"
+#include "Visuals.Menus.h"
 namespace state::in_play::AtSea
 {
 	const std::string LAYOUT_NAME = "State.InPlay.AtSea";
 
-	const std::string TEXT_AVATAR_TURNS = "avatar-turns";
-	const std::string TEXT_AVATAR_HEALTH = "avatar-health";
-	const std::string TEXT_AVATAR_HUNGER = "avatar-hunger";
-	const std::string TEXT_AVATAR_HEADING = "avatar-heading";
-	const std::string TEXT_AVATAR_SPEED = "avatar-speed";
-	const std::string TEXT_AVATAR_MONEY = "avatar-money";
-	const std::string TEXT_AVATAR_REPUTATION = "avatar-reputation";
-
 	const std::string MENU_ID = "Order";
-	const std::string MENU_ITEM_DOCK = "Dock";
-	const std::string MENU_ITEM_JOB = "Job";
 
 	const std::string AREA_CHANGE_HEADING = "ChangeHeading";
 	const std::string AREA_CHANGE_SPEED = "ChangeSpeed";
 	const std::string AREA_MOVE = "Move";
 
-	const std::string FORMAT_TURNS = "Turns Left: {:.0f}";
-	const std::string FORMAT_MONEY= "Money: {:.0f}";
-	const std::string FORMAT_REPUTATION = "Reputation: {:.0f}";
-	const std::string FORMAT_HUNGER = "Hunger: {:.0f}";
-	const std::string FORMAT_HEALTH = "Health: {:.0f}";
-	const std::string FORMAT_HEADING = "Heading: {:.2f}";
-	const std::string FORMAT_SPEED = "Speed: {:.2f}";
-
-	const std::string IMAGE_CURRENT_HEADING = "CurrentHeading";
 	const std::string IMAGE_NEW_HEADING = "NewHeading";
-	const std::string IMAGE_DESTINATION_1 = "Destination1";
-	const std::string IMAGE_DESTINATION_2 = "Destination2";
-	const std::string IMAGE_DESTINATION_3 = "Destination3";
-	const std::string IMAGE_DESTINATION_4 = "Destination4";
-	const std::string IMAGE_QUEST_DESTINATION = "QuestDestination";
 
 	const common::XY<int> CENTER = { 64, 64 };//TODO: hardcoded
+
+	void RefreshAvatarStatus();
+	void RefreshIslands();
 
 	const size_t TICKS_TOTAL = 1000;
 	static size_t ticksLeft = TICKS_TOTAL;
@@ -91,7 +63,6 @@ namespace state::in_play::AtSea
 		SHIP
 	};
 
-	static void OnEnter();
 	static void OnUpdate(const unsigned int& ticks)
 	{
 		if (autoMoveState != AutoMoveState::OFF)
@@ -109,11 +80,6 @@ namespace state::in_play::AtSea
 		}
 	}
 
-	static void OnMove()
-	{
-		ToggleAutoMove();
-	}
-
 	static void OnDock()
 	{
 		game::Avatar::Dock();
@@ -123,7 +89,7 @@ namespace state::in_play::AtSea
 	const std::map<OrderMenuItem, std::function<void()>> activators =
 	{
 		{OrderMenuItem::CHANGE_SPEED, ::application::UIState::GoTo(::UIState::IN_PLAY_CHANGE_SPEED)},
-		{OrderMenuItem::MOVE, OnMove},
+		{OrderMenuItem::MOVE, ToggleAutoMove},
 		{OrderMenuItem::DOCK, OnDock},
 		{OrderMenuItem::HEAD_FOR, application::UIState::GoTo(::UIState::IN_PLAY_HEAD_FOR)},
 		{OrderMenuItem::JOB, application::UIState::GoTo(::UIState::IN_PLAY_CURRENT_JOB)},
@@ -145,130 +111,7 @@ namespace state::in_play::AtSea
 		{::Command::RED, ::application::UIState::GoTo(::UIState::LEAVE_PLAY) }
 	};
 
-	static void UpdateAvatarHunger()
-	{
-		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_HUNGER, std::format(FORMAT_HUNGER, game::avatar::Statistics::GetCurrent(game::avatar::Statistic::HUNGER)));
-	}
-
-	static void UpdateAvatarMoney()
-	{
-		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_MONEY, std::format(FORMAT_MONEY, game::avatar::Statistics::GetCurrent(game::avatar::Statistic::MONEY)));
-	}
-
-	static void UpdateAvatarReputation()
-	{
-		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_REPUTATION, std::format(FORMAT_REPUTATION, game::avatar::Statistics::GetCurrent(game::avatar::Statistic::REPUTATION)));
-	}
-
-	static void UpdateAvatarHealth()
-	{
-		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_HEALTH, std::format(FORMAT_HEALTH, game::avatar::Statistics::GetCurrent(game::avatar::Statistic::HEALTH)));
-	}
-
-	static void UpdateAvatarTurns()
-	{
-		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_TURNS, std::format(FORMAT_TURNS, game::avatar::Statistics::GetCurrent(game::avatar::Statistic::TURNS_REMAINING)));
-	}
-
-	static void UpdateAvatarHeading()
-	{
-		auto heading = game::Avatar::GetHeading();
-		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_HEADING, std::format(FORMAT_HEADING, heading));
-		visuals::Images::SetAngle(LAYOUT_NAME, IMAGE_CURRENT_HEADING, heading);
-	}
-
-	static void UpdateAvatarSpeed()
-	{
-		auto speed = game::Avatar::GetSpeed();
-		visuals::Texts::SetText(LAYOUT_NAME, TEXT_AVATAR_SPEED, std::format(FORMAT_SPEED, speed));
-	}
-
-	const std::map<game::avatar::Destination, std::string> destinationIdImages =
-	{
-		{game::avatar::Destination::ONE, IMAGE_DESTINATION_1},
-		{game::avatar::Destination::TWO, IMAGE_DESTINATION_2},
-		{game::avatar::Destination::THREE, IMAGE_DESTINATION_3},
-		{game::avatar::Destination::FOUR, IMAGE_DESTINATION_4}
-	};
-
-	static common::XY<double> Plot(const common::XY<double>&);
-	static void UpdateAvatarDestination(const game::avatar::Destination& destinationId)
-	{
-		auto destination = game::avatar::destination::GetDestination(destinationId);
-		auto imageId = destinationIdImages.find(destinationId)->second;
-		if (destination)
-		{
-			auto clampedDistance = game::Heading::ClampDistance(destination.value() - game::Avatar::GetLocation(), game::World::GetViewDistance()+0.5);//TODO: magic number
-			auto plot = Plot(clampedDistance);
-			visuals::Images::SetLocation(LAYOUT_NAME, imageId, { (int)plot.GetX(), (int)plot.GetY() });
-			visuals::Images::SetVisible(LAYOUT_NAME, imageId, true);
-			return;
-		}
-		visuals::Images::SetVisible(LAYOUT_NAME, imageId, false);
-	}
-
-	static void UpdateAvatarDestinations()
-	{
-		for (auto destinationId : game::avatar::destination::All())
-		{
-			UpdateAvatarDestination(destinationId);
-		}
-	}
-
-	static void UpdateAvatarQuestDestination()
-	{
-		auto quest = game::avatar::Quest::Read();
-		if (quest)
-		{
-			auto clampedDistance = game::Heading::ClampDistance(quest.value().destination - game::Avatar::GetLocation(), game::World::GetViewDistance() + 0.5);//TODO: magic number
-			auto plot = Plot(clampedDistance);
-			visuals::Images::SetLocation(LAYOUT_NAME, IMAGE_QUEST_DESTINATION, { (int)plot.GetX(), (int)plot.GetY() });
-			visuals::Images::SetVisible(LAYOUT_NAME, IMAGE_QUEST_DESTINATION, true);
-			return;
-		}
-		visuals::Images::SetVisible(LAYOUT_NAME, IMAGE_QUEST_DESTINATION, false);
-	}
-
-	static void UpdateAvatarStatus()
-	{
-		UpdateAvatarHeading();
-		UpdateAvatarHealth();
-		UpdateAvatarHunger();
-		UpdateAvatarSpeed();
-		UpdateAvatarMoney();
-		UpdateAvatarReputation();
-		UpdateAvatarTurns();
-		UpdateAvatarDestinations();
-		UpdateAvatarQuestDestination();
-		visuals::MenuItems::SetEnabled(LAYOUT_NAME, MENU_ITEM_JOB, game::avatar::Quest::Read().has_value());
-	}
-
-	//TODO: get this not hardcoded
-	const common::XY<double> VIEW_CENTER = { 162.0,182.0 };
-	const double VIEW_RADIUS = 144.0;
-	const int ISLAND_ICON_COUNT = 10;
-	const int IMAGE_OFFSET_X = 0;
-	const int IMAGE_OFFSET_Y = 0;
-	const int TEXT_OFFSET_X = 0;
-	const int TEXT_OFFSET_Y = 8;
-
-	static void HideVisibleIslands()
-	{
-		for (int icon = 0; icon < ISLAND_ICON_COUNT; ++icon)
-		{
-			auto visualId = std::format("AtSeaIsland{}", icon);
-			visuals::Images::SetVisible(LAYOUT_NAME, visualId, false);
-			visuals::Texts::SetText(LAYOUT_NAME, visualId, "");
-		}
-	}
-
-	static common::XY<double> Plot(const common::XY<double>& location)
-	{
-		double viewScale = VIEW_RADIUS / game::World::GetViewDistance();
-		return location* viewScale + VIEW_CENTER;
-	}
-
-	static void RefreshAutoMoveState(bool canDock)
+	void UpdateAutoMoveState(bool canDock)
 	{
 		if (canDock)
 		{
@@ -286,35 +129,12 @@ namespace state::in_play::AtSea
 		}
 	}
 
-	static void UpdateIslands()
-	{
-		HideVisibleIslands();
-		auto islands = game::Islands::GetViewableIslands();
-		int icon = 0;
-		double dockDistance = game::World::GetDockDistance();
-		bool canDock = false;
-		for (auto& entry : islands)
-		{
-			canDock |= (game::Heading::Distance(entry.relativeLocation, { 0.0, 0.0 }) <= dockDistance);
-			auto plot = Plot(entry.relativeLocation);
-			auto visualId = std::format("AtSeaIsland{}", icon);
-			visuals::Images::SetLocation(LAYOUT_NAME, visualId, { (int)plot.GetX() + IMAGE_OFFSET_X,(int)plot.GetY() + IMAGE_OFFSET_Y });
-			visuals::Images::SetVisible(LAYOUT_NAME, visualId, true);
-			visuals::Texts::SetLocation(LAYOUT_NAME, visualId, { (int)plot.GetX() + TEXT_OFFSET_X,(int)plot.GetY() + TEXT_OFFSET_Y });
-			visuals::Texts::SetText(LAYOUT_NAME, visualId, (entry.visits.has_value()) ? (entry.name) : ("????"));
-			++icon;
-		}
-		visuals::MenuItems::SetEnabled(LAYOUT_NAME, MENU_ITEM_DOCK, canDock);
-		RefreshAutoMoveState(canDock);
-	}
-
 	static void OnEnter()
 	{
 		game::audio::Mux::Play(game::audio::Mux::Theme::MAIN);
-		UpdateAvatarStatus();
-		UpdateIslands();
+		RefreshAvatarStatus();
+		RefreshIslands();
 	}
-
 
 	static void OnMouseMotionInHelm(const common::XY<int>& location)
 	{
@@ -344,7 +164,7 @@ namespace state::in_play::AtSea
 		if (areaName == AREA_CHANGE_HEADING)
 		{
 			game::Avatar::SetHeading(newHeading);
-			UpdateAvatarStatus();
+			RefreshAvatarStatus();
 			return true;
 		}
 		return false;
