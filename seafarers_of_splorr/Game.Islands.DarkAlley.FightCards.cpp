05b0160412b4adf0f0ce4338713ebf1cbc7b2fe4
ernestpazera <ernestpazera@gmail.com>
2021-08-14 06:49:04 -0500

    refactored the fightcard generation function

--- seafarers_of_splorr/Game.Islands.DarkAlley.FightCards.cpp
+++ seafarers_of_splorr/Game.Islands.DarkAlley.FightCards.cpp
@@ -63,85 +63,111 @@ namespace game::islands::dark_alley::FightCards
 		{11, {6,7,10}}
 	};
 
+	static void WriteCards(const common::XY<double>& location, const std::map<size_t, DarkAlley::FightCard>& fightCards)
+	{
+		for (auto& fightCard : fightCards)
+		{
+			data::game::island::dark_alley::FightCard::Write(location, fightCard.first,
+				{
+					(int)std::get<0>(fightCard.second.card),
+					(int)std::get<1>(fightCard.second.card),
+					fightCard.second.adjacent,
+					fightCard.second.shown
+				});
+		}
+	}
+
+	static size_t DetermineFaceCardCount(const data::game::island::DarkAlley::DarkAlleyData& darkAlley)
+	{
+		auto brawling = game::avatar::Statistics::GetBrawling();
+		size_t faceCardCount =
+			(size_t)(brawling / (brawling + darkAlley.ruffianBrawlingStrength) * (double)CARD_COUNT);
+		return
+			(faceCardCount < 1) ? (1) :
+			(faceCardCount > CARD_COUNT - 1) ? (CARD_COUNT - 1) :
+			(faceCardCount);
+	}
+
+	static common::Card GenerateCard(const std::list<common::card::Rank>& ranks)
+	{
+		auto rank = common::RNG::FromList(ranks).value();
+		auto suit = common::RNG::FromList(CARD_SUITS).value();
+		return std::make_tuple(rank, suit);
+	}
+
+	void PlaceFaceCards(std::map<size_t, DarkAlley::FightCard>& fightCards, std::set<common::Card>& cards, const data::game::island::DarkAlley::DarkAlleyData& darkAlley)
+	{
+		size_t faceCardCount = DetermineFaceCardCount(darkAlley);
+		while (faceCardCount > 0)
+		{
+			auto card = GenerateCard(FACE_CARD_RANKS);
+			if (!cards.contains(card))
+			{
+				cards.insert(card);
+				size_t index;
+				do
+				{
+					index = common::RNG::FromRange(0u, CARD_COUNT);
+				} while (fightCards.find(index) != fightCards.end());
+				fightCards[index] =
+				{
+					card,
+					0,
+					false
+				};
+				faceCardCount--;
+			}
+		}
+	}
+
+	void PlaceNonfaceCards(std::map<size_t, DarkAlley::FightCard>& fightCards, std::set<common::Card>& cards)
+	{
+		for (size_t index = 0; index < CARD_COUNT; ++index)
+		{
+			if (fightCards.find(index) == fightCards.end())
+			{
+				auto card = GenerateCard(NONFACE_CARD_RANKS);
+				while (cards.contains(card))
+				{
+					card = GenerateCard(NONFACE_CARD_RANKS);
+				}
+				cards.insert(card);
+				auto adjacencies = ADJACENCIES.find(index)->second;
+				int count = 0;
+				for (auto adjacency : adjacencies)
+				{
+					auto neighbor = fightCards.find(adjacency);
+					if (neighbor != fightCards.end() && IsFaceCard(neighbor->second.card))
+					{
+						count++;
+					}
+				}
+				fightCards[index] =
+				{
+					card,
+					count,
+					false
+				};
+			}
+		}
+	}
+
+	void DoGenerate(const common::XY<double>& location, const data::game::island::DarkAlley::DarkAlleyData& darkAlley)
+	{
+		std::map<size_t, DarkAlley::FightCard> fightCards;
+		std::set<common::Card> cards;
+		PlaceFaceCards(fightCards, cards, darkAlley);
+		PlaceNonfaceCards(fightCards, cards);
+		WriteCards(location, fightCards);
+	}
+
 	void Generate(const common::XY<double>& location)
 	{
 		data::game::island::dark_alley::FightCard::Clear(location);
 		auto darkAlley = data::game::island::DarkAlley::Read(location);
 		if (darkAlley)
 		{
-			auto brawling = game::avatar::Statistics::GetBrawling();
-			size_t faceCardCount = 
-				(size_t)(brawling / (brawling + darkAlley.value().ruffianBrawlingStrength) * (double)CARD_COUNT);
-			faceCardCount =
-				(faceCardCount < 1) ? (1) :
-				(faceCardCount > CARD_COUNT - 1) ? (CARD_COUNT - 1) :
-				(faceCardCount);
-			std::map<size_t, DarkAlley::FightCard> fightCards;
-			std::set<common::Card> cards;
-			while (faceCardCount > 0)
-			{
-				auto rank = common::RNG::FromList(FACE_CARD_RANKS).value();
-				auto suit = common::RNG::FromList(CARD_SUITS).value();
-				auto card = std::make_tuple(rank, suit);
-				if (!cards.contains(card))
-				{
-					cards.insert(card);
-					size_t index;
-					do
-					{
-						index = common::RNG::FromRange(0u, CARD_COUNT);
-					} while (fightCards.find(index) != fightCards.end());
-					fightCards[index] = 
-					{
-						card,
-						0,
-						false
-					};
-					faceCardCount--;
-				}
-			}
-			for (size_t index = 0; index < CARD_COUNT; ++index)
-			{
-				if (fightCards.find(index) == fightCards.end())
-				{
-					auto rank = common::RNG::FromList(NONFACE_CARD_RANKS).value();
-					auto suit = common::RNG::FromList(CARD_SUITS).value();
-					auto card = std::make_tuple(rank, suit);
-					while (cards.contains(card))
-					{
-						rank = common::RNG::FromList(NONFACE_CARD_RANKS).value();
-						suit = common::RNG::FromList(CARD_SUITS).value();
-						card = std::make_tuple(rank, suit);
-					}
-					cards.insert(card);
-					auto adjacencies = ADJACENCIES.find(index)->second;
-					int count = 0;
-					for (auto adjacency : adjacencies)
-					{
-						auto neighbor = fightCards.find(adjacency);
-						if (neighbor != fightCards.end() && IsFaceCard(neighbor->second.card))
-						{
-							count++;
-						}
-					}
-					fightCards[index]= 
-					{
-						card,
-						count,
-						false
-					};
-				}
-			}
-			for (auto& fightCard : fightCards)
-			{
-				data::game::island::dark_alley::FightCard::Write(location, fightCard.first, 
-					{
-						(int)std::get<0>(fightCard.second.card),
-						(int)std::get<1>(fightCard.second.card),
-						fightCard.second.adjacent,
-						fightCard.second.shown
-					});
-			}
+			DoGenerate(location, darkAlley.value());
 		}
 	}
 
