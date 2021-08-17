9f15b893ef634aafd70bc13e593707fd99ea854c
ernestpazera <ernestpazera@gmail.com>
2021-08-17 14:05:20 -0500

    split out stores

--- seafarers_of_splorr/Data.SQLite.Stores.h
+++ seafarers_of_splorr/Data.SQLite.Stores.h
@@ -2,19 +2,7 @@
 #include <string>
 #include <map>
 #include <list>
-namespace data::sqlite
-{
-	enum class Store
-	{
-		IN_MEMORY,
-		AUTOSAVE,
-		SLOT_1,
-		SLOT_2,
-		SLOT_3,
-		SLOT_4,
-		SLOT_5
-	};
-}
+#include "Data.SQLite.Store.h"
 namespace data::sqlite::Stores
 {
 	std::list<std::map<std::string, std::string>> Execute(const data::sqlite::Store&, const std::string&);
