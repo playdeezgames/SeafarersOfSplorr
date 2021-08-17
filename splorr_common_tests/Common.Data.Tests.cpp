babbe1321f6ab1baf75c08832fabf42f8942d4bc
ernestpazera <ernestpazera@gmail.com>
2021-08-17 05:37:32 -0500

    tooptionaldouble

--- splorr_common_tests/Common.Data.Tests.cpp
+++ splorr_common_tests/Common.Data.Tests.cpp
@@ -25,5 +25,57 @@ namespace common::Data
 				auto actual = ToDouble("1.2");
 				Assert(actual == 1.2, "Actual should be 1.2");
 			}),
+		AddTest(
+			"common::Data::ToInt should return 0 when given garbage",
+			[]()
+			{
+				auto actual = ToInt("GARBAGE");
+				Assert(actual == 0, "Actual should be zero");
+			}),
+		AddTest(
+			"common::Data::ToInt should parse a valid int",
+			[]()
+			{
+				auto actual = ToInt("1");
+				Assert(actual == 1, "Actual should be 1");
+			}),
+		AddTest(
+			"common::Data::ToOptionalInt should return nullopt when given empty string",
+			[]()
+			{
+				auto actual = ToOptionalInt("");
+				Assert(!actual.has_value(), "Actual should should not have a value");
+			}),
+		AddTest(
+			"common::Data::ToOptionalInt should parse a valid int",
+			[]()
+			{
+				auto actual = ToOptionalInt("1");
+				Assert(actual.has_value(), "Actual should have a value");
+				Assert(actual.value()==1, "Actual should have a value of 1");
+			}),
+		AddTest(
+			"common::Data::ToOptionalDouble should return nullopt when given empty string",
+			[]()
+			{
+				auto actual = ToOptionalDouble("");
+				Assert(!actual.has_value(), "Actual should should not have a value");
+			}),
+		AddTest(
+			"common::Data::ToOptionalDouble should parse a valid int",
+			[]()
+			{
+				auto actual = ToOptionalDouble("1");
+				Assert(actual.has_value(), "Actual should have a value");
+				Assert(actual.value() == 1.0, "Actual should have a value of 1.0");
+			}),
+		AddTest(
+			"common::Data::ToOptionalDouble should parse a valid double",
+			[]()
+			{
+				auto actual = ToOptionalDouble("1.2");
+				Assert(actual.has_value(), "Actual should have a value");
+				Assert(actual.value() == 1.2, "Actual should have a value of 1.2");
+			}),
 	};
 }
