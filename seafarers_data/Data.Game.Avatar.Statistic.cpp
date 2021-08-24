2432cfbe10fb5d2fdbbc8f8f0e6402f81c26c0f6
ernestpazera <ernestpazera@gmail.com>
2021-08-24 04:32:20 -0500

    statistics now have avatar id

--- seafarers_data/Data.Game.Avatar.Statistic.cpp
+++ seafarers_data/Data.Game.Avatar.Statistic.cpp
@@ -8,23 +8,23 @@ namespace data::game::avatar
 	static const std::string FIELD_MINIMUM = "Minimum";
 	static const std::string FIELD_MAXIMUM = "Maximum";
 	static const std::string FIELD_CURRENT = "Current";
-	static const std::string CREATE_TABLE = "CREATE TABLE IF NOT EXISTS [AvatarStatistics]([StatisticId] INT NOT NULL UNIQUE,[Minimum] REAL NULL,[Maximum] REAL NULL,[Current] REAL NOT NULL);";
-	static const std::string QUERY_ITEM = "SELECT [Minimum], [Maximum], [Current] FROM [AvatarStatistics] WHERE [StatisticId] = {};";
-	static const std::string REPLACE_ITEM = "REPLACE INTO [AvatarStatistics]([StatisticId],[Minimum],[Maximum],[Current]) VALUES ({},{},{},{});";
+	static const std::string CREATE_TABLE = "CREATE TABLE IF NOT EXISTS [AvatarStatistics]([AvatarId] INT NOT NULL,[StatisticId] INT NOT NULL,[Minimum] REAL NULL,[Maximum] REAL NULL,[Current] REAL NOT NULL, UNIQUE([AvatarId],[StatisticId]));";
+	static const std::string QUERY_ITEM = "SELECT [Minimum], [Maximum], [Current] FROM [AvatarStatistics] WHERE [StatisticId] = {} AND [AvatarId]={};";
+	static const std::string REPLACE_ITEM = "REPLACE INTO [AvatarStatistics]([AvatarId],[StatisticId],[Minimum],[Maximum],[Current]) VALUES ({},{},{},{},{});";
 
 	static const auto AutoCreateAvatarStatisticsTable = data::game::Common::Run(CREATE_TABLE);
 
 	void Statistic::Write(int statisticId, const Statistic& data)
 	{
 		AutoCreateAvatarStatisticsTable();
-		auto query = std::format(REPLACE_ITEM, statisticId, common::Data::OfOptional(data.minimum), common::Data::OfOptional(data.maximum), data.current);
+		auto query = std::format(REPLACE_ITEM, Common::AVATAR_ID, statisticId, common::Data::OfOptional(data.minimum), common::Data::OfOptional(data.maximum), data.current);
 		data::game::Common::Execute(query);
 	}
 
 	std::optional<Statistic> Statistic::Read(int statisticId)
 	{
 		AutoCreateAvatarStatisticsTable();
-		auto query = std::format(QUERY_ITEM, statisticId);
+		auto query = std::format(QUERY_ITEM, statisticId, Common::AVATAR_ID);
 		auto result = data::game::Common::Execute(query);
 		if (!result.empty())
 		{
