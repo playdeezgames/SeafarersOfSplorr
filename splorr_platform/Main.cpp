0d5098f1277afcbca8839f85738fd7511fd5934b
ernestpazera <ernestpazera@gmail.com>
2021-09-05 11:15:01 -0500

    moving items out int the platform lib

--- splorr_platform/Main.cpp
+++ splorr_platform/Main.cpp
@@ -1,21 +1,7 @@
 #include <Application.Engine.h>
-#include "Common.RNG.h"
-#include "Common.Utility.h"
-#include "Game.Achievements.h"
+#include <Common.RNG.h>
+#include <Common.Utility.h>
 #include <SDL.h>
-namespace application
-{
-	bool Engine::ShouldStart()
-	{
-		if (game::Achievements::ShouldRestartApp())
-		{
-			return false;
-		}
-		return true;
-	}
-	const std::string Engine::CONFIG_FILE = "config/ui/application.json";
-}
-//TODO: move vv to splorr_platform
 static std::vector<std::string> ParseCommandLine(int argc, char** argv)
 {
 	std::vector<std::string> arguments;
