8c319f7737d6a5601c8cad9d49149092d09ac269
ernestpazera <ernestpazera@gmail.com>
2021-09-23 04:46:10 -0500

    fixed up fishboardcells

--- seafarers_data/Data.Game.FishboardCell.cpp
+++ seafarers_data/Data.Game.FishboardCell.cpp
@@ -4,27 +4,31 @@
 #include <format>
 namespace data::game
 {
-	static const std::string CREATE_TABLE = "CREATE TABLE IF NOT EXISTS [FishBoards]([AvatarId] INT NOT NULL, [X] INT NOT NULL, [Y] INT NOT NULL, [Revealed] INT NOT NULL, [FishType] INT NULL, UNIQUE([AvatarId],[X],[Y]));";
-	static const std::string DELETE_ALL = "DELETE FROM [FishBoards] WHERE [AvatarId]={};";
-	static const std::string REPLACE_ITEM = "REPLACE INTO [FishBoards] ([AvatarId],[X],[Y],[Revealed],[FishType]) VALUES ({},{},{},{},{});";
-	static const std::string QUERY_ALL = "SELECT [X],[Y],[Revealed],[FishType] FROM [FishBoards] WHERE [AvatarId]={};";
-	static const std::string QUERY_ITEM = "SELECT [X],[Y],[Revealed],[FishType] FROM [FishBoards] WHERE [AvatarId]={} AND [X]={} AND [Y]={};";
+	static const std::string CREATE_TABLE = "CREATE TABLE IF NOT EXISTS [FishboardCells]([AvatarId] INT NOT NULL, [X] INT NOT NULL, [Y] INT NOT NULL, [Revealed] INT NOT NULL, [FishType] INT NULL, UNIQUE([AvatarId],[X],[Y]));";
+	static const std::string DELETE_ALL = "DELETE FROM [FishboardCells] WHERE [AvatarId]={};";
+	static const std::string REPLACE_ITEM = "REPLACE INTO [FishboardCells] ([AvatarId],[X],[Y],[Revealed],[FishType]) VALUES ({},{},{},{},{});";
+	static const std::string QUERY_ALL = "SELECT [X],[Y],[Revealed],[FishType] FROM [FishboardCells] WHERE [AvatarId]={};";
+	static const std::string QUERY_ITEM = "SELECT [X],[Y],[Revealed],[FishType] FROM [FishboardCells] WHERE [AvatarId]={} AND [X]={} AND [Y]={};";
+	static const std::string QUERY_FISH_COUNT = "SELECT COUNT([FishType]) AS [FishCount] FROM [FishboardCells] WHERE [AvatarId]={} AND [FishType] IS NOT NULL;";
+	static const std::string QUERY_REVEALED_FISH_COUNT = "SELECT COUNT([FishType]) AS [FishCount] FROM [FishboardCells] WHERE [AvatarId]={} AND [FishType] IS NOT NULL AND [Revealed]<>0;";
+	static const std::string QUERY_FISHES = "SELECT DISTINCT [FishType] FROM [FishboardCells] WHERE [AvatarId]={} AND [FishType] IS NOT NULL;";
 	static const std::string FIELD_X = "X";
 	static const std::string FIELD_Y = "Y";
 	static const std::string FIELD_REVEALED = "Revealed";
 	static const std::string FIELD_FISH_TYPE = "FishType";
+	static const std::string FIELD_FISH_COUNT = "FishCount";
 
-	static const auto AutoCreateFishBoardTable = Common::Run(CREATE_TABLE);
+	static const auto AutoCreateFishboardCellsTable = Common::Run(CREATE_TABLE);
 
 	void FishboardCell::Clear()
 	{
-		AutoCreateFishBoardTable();
+		AutoCreateFishboardCellsTable();
 		Common::Execute(std::format(DELETE_ALL, Common::AVATAR_ID));
 	}
 
 	void FishboardCell::Write(const FishboardCell& cell)
 	{
-		AutoCreateFishBoardTable();
+		AutoCreateFishboardCellsTable();
 		Common::Execute(
 			std::format(
 				REPLACE_ITEM,
@@ -49,7 +53,7 @@ namespace data::game
 
 	std::list<FishboardCell> FishboardCell::All()
 	{
-		AutoCreateFishBoardTable();
+		AutoCreateFishboardCellsTable();
 		auto records = Common::Execute(std::format(QUERY_ALL, Common::AVATAR_ID));
 		std::list<FishboardCell> results;
 		for (auto& record : records)
@@ -61,7 +65,7 @@ namespace data::game
 
 	std::optional<FishboardCell> FishboardCell::Read(const common::XY<int>& location)
 	{
-		AutoCreateFishBoardTable();
+		AutoCreateFishboardCellsTable();
 		auto records = Common::Execute(std::format(QUERY_ITEM, Common::AVATAR_ID, location.GetX(), location.GetY()));
 		if (!records.empty())
 		{
@@ -72,41 +76,35 @@ namespace data::game
 
 	size_t FishboardCell::ReadFishCount()
 	{
-		size_t count = 0;
-		for (auto cell : All())
+		AutoCreateFishboardCellsTable();
+		auto records = Common::Execute(std::format(QUERY_FISH_COUNT, Common::AVATAR_ID));
+		if (!records.empty())
 		{
-			if (cell.fishType)
-			{
-				count++;
-			}
+			return common::Data::ToInt(records.front().find(FIELD_FISH_COUNT)->second);
 		}
-		return count;
+		return 0;
 	}
 
 	size_t FishboardCell::ReadRevealedFishCount()
 	{
-		size_t count = 0;
-		for (auto cell : All())
+		AutoCreateFishboardCellsTable();
+		auto records = Common::Execute(std::format(QUERY_REVEALED_FISH_COUNT, Common::AVATAR_ID));
+		if (!records.empty())
 		{
-			if (cell.fishType && cell.revealed)
-			{
-				count++;
-			}
+			return common::Data::ToInt(records.front().find(FIELD_FISH_COUNT)->second);
 		}
-		return count;
+		return 0;
 	}
 
 	std::set<int> FishboardCell::ReadFish()
 	{
 		std::set<int> result;
-		for (auto cell : All())
+		AutoCreateFishboardCellsTable();
+		auto records = Common::Execute(std::format(QUERY_FISHES, Common::AVATAR_ID));
+		for (auto cell : records)
 		{
-			if (cell.fishType && !result.contains(cell.fishType.value()))
-			{
-				result.insert(cell.fishType.value());
-			}
+			result.insert(common::Data::ToInt(cell.find(FIELD_FISH_TYPE)->second));
 		}
 		return result;
 	}
-
 }
