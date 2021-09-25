51864d5e02c0185a65a13c8fc93d2995f25966ec
ernestpazera <ernestpazera@gmail.com>
2021-09-25 04:17:24 -0500

    avatar equipment

--- seafarers_data/Data.Game.Avatar.Equipment.cpp
+++ seafarers_data/Data.Game.Avatar.Equipment.cpp
@@ -15,10 +15,10 @@ namespace data::game::avatar
 
 	static auto AutoCreateAvatarEquipmentTable = Common::Run(CREATE_TABLE);
 
-	std::optional<int> Equipment::Read(int equipSlotId)
+	std::optional<int> Equipment::Read(int avatarId, int equipSlotId)
 	{
 		AutoCreateAvatarEquipmentTable();
-		auto records = Common::Execute(std::format(QUERY_ITEM, Common::AVATAR_ID, equipSlotId));
+		auto records = Common::Execute(std::format(QUERY_ITEM, avatarId, equipSlotId));
 		if (!records.empty())
 		{
 			return common::Data::ToOptionalInt(records.front()[FIELD_ITEM_ID]);
@@ -26,32 +26,52 @@ namespace data::game::avatar
 		return std::nullopt;
 	}
 
-	void Equipment::Write(int equipSlotId, const std::optional<int>& itemId)
+	void Equipment::Write(int avatarId, int equipSlotId, const std::optional<int>& itemId)
 	{
 		AutoCreateAvatarEquipmentTable();
 		if (itemId)
 		{
-			Common::Execute(std::format(REPLACE_ITEM, Common::AVATAR_ID, equipSlotId, itemId.value()));
+			Common::Execute(std::format(REPLACE_ITEM, avatarId, equipSlotId, itemId.value()));
 			return;
 		}
-		Common::Execute(std::format(DELETE_ITEM, Common::AVATAR_ID, equipSlotId));
+		Common::Execute(std::format(DELETE_ITEM, avatarId, equipSlotId));
 	}
 
-	void Equipment::Clear()
+	void Equipment::Clear(int avatarId)
 	{
 		AutoCreateAvatarEquipmentTable();
-		Common::Execute(std::format(DELETE_ALL, Common::AVATAR_ID));
+		Common::Execute(std::format(DELETE_ALL, avatarId));
 	}
 
-	std::map<int, int> Equipment::All()
+	std::map<int, int> Equipment::All(int avatarId)
 	{
 		std::map<int, int> result;
 		AutoCreateAvatarEquipmentTable();
-		auto records = Common::Execute(std::format(QUERY_ALL, Common::AVATAR_ID));
+		auto records = Common::Execute(std::format(QUERY_ALL, avatarId));
 		for (auto& record : records)
 		{
 			result[common::Data::ToInt(record[FIELD_EQUIP_SLOT_ID])] = common::Data::ToInt(record[FIELD_ITEM_ID]);
 		}
 		return result;
+	}
+
+	std::optional<int> Equipment::Read(int equipSlotId) 
+	{
+		return Read(Common::AVATAR_ID, equipSlotId);
+	}
+
+	void Equipment::Write(int equipSlotId, const std::optional<int>& itemId) 
+	{
+		Write(Common::AVATAR_ID, equipSlotId, itemId);
+	}
+
+	void Equipment::Clear() 
+	{
+		Clear(Common::AVATAR_ID);
+	}
+
+	std::map<int, int> Equipment::All() 
+	{
+		return All(Common::AVATAR_ID);
 	}
 }
--- seafarers_data/Data.Game.Avatar.Equipment.h
+++ seafarers_data/Data.Game.Avatar.Equipment.h
@@ -5,6 +5,10 @@ namespace data::game::avatar
 {
 	struct Equipment
 	{
+		static std::optional<int> Read(int, int);
+		static void Write(int, int, const std::optional<int>&);
+		static void Clear(int);
+		static std::map<int, int> All(int);
 		static std::optional<int> Read(int);
 		static void Write(int, const std::optional<int>&);
 		static void Clear();
