0ab44a2e20ac69083d44da1f041b1873751fb5b8
ernestpazera <ernestpazera@gmail.com>
2021-09-25 04:06:32 -0500

    avatar items

--- seafarers_data/Data.Game.Avatar.Items.cpp
+++ seafarers_data/Data.Game.Avatar.Items.cpp
@@ -10,16 +10,17 @@ namespace data::game::avatar
 	static const std::string REPLACE_ITEM = "REPLACE INTO [AvatarItems]([AvatarId],[ItemId],[ItemCount]) VALUES({},{},{});";
 	static const std::string QUERY_ITEM = "SELECT [ItemCount] FROM [AvatarItems] WHERE [AvatarId]={} AND [ItemId]={};";
 	static const std::string QUERY_ALL = "SELECT [ItemId],[ItemCount] FROM [AvatarItems] WHERE [AvatarId]={};";
-	static const std::string DELETE_ALL = "DELETE FROM [AvatarItems];";
+	static const std::string DELETE_ALL = "DELETE FROM [AvatarItems] WHERE [AvatarId]={};";
+	static const std::string DELETE_ALL_ITEMS = "DELETE FROM [AvatarItems];";
 	static const std::string DELETE_ITEM = "DELETE FROM [AvatarItems] WHERE [AvatarId]={} AND [ItemId]={};";
 
 	static const auto AutoCreateAvatarItemsTable = data::game::Common::Run(CREATE_TABLE);
 
-	size_t Items::Read(const int& itemId)
+	size_t Items::Read(int avatarId, const int& itemId)
 	{
 		AutoCreateAvatarItemsTable();
 		auto records =
-			data::game::Common::Execute(std::format(QUERY_ITEM,data::game::Common::AVATAR_ID, itemId));
+			data::game::Common::Execute(std::format(QUERY_ITEM, avatarId, itemId));
 		if (!records.empty())
 		{
 			auto& record = records.front();
@@ -28,31 +29,57 @@ namespace data::game::avatar
 		return 0;
 	}
 
-	void Items::Write(const int& itemId, const size_t& count)
+	void Items::Write(int avatarId, const int& itemId, const size_t& count)
 	{
 		AutoCreateAvatarItemsTable();
-		data::game::Common::Execute(std::format(DELETE_ITEM, data::game::Common::AVATAR_ID, itemId));
+		data::game::Common::Execute(std::format(DELETE_ITEM, avatarId, itemId));
 		if (count > 0)
 		{
-			data::game::Common::Execute(std::format(REPLACE_ITEM, data::game::Common::AVATAR_ID, itemId, count));
+			data::game::Common::Execute(std::format(REPLACE_ITEM, avatarId, itemId, count));
 		}
 	}
 
-	void Items::Clear()
+	void Items::Clear(int avatarId)
 	{
 		AutoCreateAvatarItemsTable();
-		data::game::Common::Execute(DELETE_ALL);
+		data::game::Common::Execute(std::format(DELETE_ALL, avatarId));
 	}
 
-	std::map<int, size_t> Items::All()
+	void Items::ClearAll()
+	{
+		AutoCreateAvatarItemsTable();
+		data::game::Common::Execute(DELETE_ALL_ITEMS);
+	}
+
+	std::map<int, size_t> Items::All(int avatarId)
 	{
 		std::map<int, size_t> result;
 		auto records =
-			data::game::Common::Execute(std::format(QUERY_ALL, data::game::Common::AVATAR_ID));
+			data::game::Common::Execute(std::format(QUERY_ALL, avatarId));
 		for (auto& record : records)
 		{
 			result[common::Data::ToInt(record[FIELD_ITEM_ID])] = (size_t)common::Data::ToInt(record[FIELD_ITEM_COUNT]);
 		}
 		return result;
+	}
+
+	size_t Items::Read(const int& itemId)
+	{
+		return Read(Common::AVATAR_ID, itemId);
+	}
+
+	std::map<int, size_t> Items::All()
+	{
+		return All(Common::AVATAR_ID);
+	}
+
+	void Items::Write(const int& itemId, const size_t& itemCount)
+	{
+		Write(Common::AVATAR_ID, itemId, itemCount);
+	}
+
+	void Items::Clear()
+	{
+		Clear(Common::AVATAR_ID);
 	}
 }
--- seafarers_data/Data.Game.Avatar.Items.h
+++ seafarers_data/Data.Game.Avatar.Items.h
@@ -4,9 +4,14 @@ namespace data::game::avatar
 {
 	struct Items
 	{
+		static size_t Read(int, const int&);
+		static std::map<int, size_t> All(int);
+		static void Write(int, const int&, const size_t&);
+		static void Clear(int);
 		static size_t Read(const int&);
 		static std::map<int, size_t> All();
 		static void Write(const int&, const size_t&);
 		static void Clear();
+		static void ClearAll();
 	};
 }
