d3c4586d43c18261edb057df5aa59f091daf4ccf
ernestpazera <ernestpazera@gmail.com>
2021-09-25 03:58:41 -0500

    avatar logs

--- seafarers_data/Data.Game.Avatar.Log.cpp
+++ seafarers_data/Data.Game.Avatar.Log.cpp
@@ -8,28 +8,29 @@ namespace data::game::avatar
 	static const std::string FIELD_LOG_TEXT = "LogText";
 	static const std::string CREATE_TABLE = "CREATE TABLE IF NOT EXISTS [AvatarLogs]([LogId] INTEGER PRIMARY KEY AUTOINCREMENT, [AvatarId] INT NOT NULL, [LogColor] TEXT NOT NULL, [LogText] TEXT NOT NULL);";
 	static const std::string DELETE_ALL = "DELETE FROM [AvatarLogs] WHERE [AvatarId]={};";
+	static const std::string DELETE_ALL_LOGS = "DELETE FROM [AvatarLogs];";
 	static const std::string QUERY_ALL = "SELECT [LogColor], [LogText] FROM [AvatarLogs] WHERE [AvatarId]={} ORDER BY [LogId] DESC LIMIT {};";
 	static const std::string INSERT_ITEM = "INSERT INTO [AvatarLogs]([AvatarId],[LogColor],[LogText]) VALUES({},{},{});";
 
 	static const auto AutoCreateAvatarLogTable = data::game::Common::Run(CREATE_TABLE);
 
-	void Log::Clear()
+	void Log::Clear(int avatarId)
 	{
 		AutoCreateAvatarLogTable();
-		Common::Execute(std::format(DELETE_ALL, Common::AVATAR_ID));
+		Common::Execute(std::format(DELETE_ALL, avatarId));
 	}
 
-	void Log::Write(const std::string& color, const std::string& text)
+	void Log::Write(int avatarId,const std::string& color, const std::string& text)
 	{
 		AutoCreateAvatarLogTable();
-		Common::Execute(std::format(INSERT_ITEM, Common::AVATAR_ID, common::Data::QuoteString(color), common::Data::QuoteString(text)));
+		Common::Execute(std::format(INSERT_ITEM, avatarId, common::Data::QuoteString(color), common::Data::QuoteString(text)));
 	}
 
-	std::list<std::tuple<std::string, std::string>> Log::Read(size_t entryCount)
+	std::list<std::tuple<std::string, std::string>> Log::Read(int avatarId,size_t entryCount)
 	{
 		AutoCreateAvatarLogTable();
 		std::list<std::tuple<std::string, std::string>> results;
-		auto records = Common::Execute(std::format(QUERY_ALL, Common::AVATAR_ID, entryCount));
+		auto records = Common::Execute(std::format(QUERY_ALL, avatarId, entryCount));
 		for (auto record : records)
 		{
 			results.push_back(
@@ -38,5 +39,25 @@ namespace data::game::avatar
 					record[FIELD_LOG_TEXT]));
 		}
 		return results;
+	}
+	void Log::Clear()
+	{
+		Clear(Common::AVATAR_ID);
+	}
+
+	void Log::Write(const std::string& color, const std::string& text)
+	{
+		Write(Common::AVATAR_ID, color, text);
+	}
+
+	std::list<std::tuple<std::string, std::string>> Log::Read(size_t entryCount)
+	{
+		return Read(Common::AVATAR_ID, entryCount);
+	}
+
+	void Log::ClearAll()
+	{
+		AutoCreateAvatarLogTable();
+		Common::Execute(std::format(DELETE_ALL_LOGS));
 	}
 }
--- seafarers_data/Data.Game.Avatar.Log.h
+++ seafarers_data/Data.Game.Avatar.Log.h
@@ -6,8 +6,12 @@ namespace data::game::avatar
 {
 	struct Log
 	{
+		static void Clear(int);
+		static void Write(int,const std::string&, const std::string&);
+		static std::list<std::tuple<std::string, std::string>> Read(int,size_t);
 		static void Clear();
 		static void Write(const std::string&, const std::string&);
 		static std::list<std::tuple<std::string, std::string>> Read(size_t);
+		static void ClearAll();
 	};
 }
