f92f624afa2a49e1a79a1ab420d90e1abf96658f
ernestpazera <ernestpazera@gmail.com>
2021-10-13 05:31:16 -0500

    fishboarstate -> fishgamestate header

--- seafarers_game/Game.Fishboard.h
+++ seafarers_game/Game.Fishboard.h
@@ -2,7 +2,7 @@
 #include <Common.XY.h>
 #include "Game.Fish.h"
 #include "Game.FishboardCell.h"
-#include "Game.FishboardState.h"
+#include "Game.FishGameState.h"
 #include <optional>
 namespace game//20211013
 {
