9d7bf13a25ad762ff51f11ddc9f1507190bacbe2
ernestpazera <ernestpazera@gmail.com>
2021-10-15 05:21:55 -0500

    game commodities

--- seafarers_game/Game.Commodities.cpp
+++ seafarers_game/Game.Commodities.cpp
@@ -2,7 +2,7 @@
 #include "Game.Commodities.h"
 #include <map>
 #include <string>
-namespace game
+namespace game//20211015
 {
 	struct CommodityDescriptor
 	{
@@ -15,15 +15,96 @@ namespace game
 
 	static const std::map<game::Commodity, game::CommodityDescriptor> commodityTable =
 	{
-		{game::Commodity::WHEAT, { "wheat", 1.0, 0.05, 0.05, 0.15}},
-		{game::Commodity::WOOD, { "wood", 7.5, 0.05, 0.05, 0.15}},
-		{game::Commodity::WOOL, { "wool", 2.5, 0.05, 0.05, 0.15}},
-		{game::Commodity::LABOR, { "labor", 1.0, 0.05, 0.05, 0.15}},
-		{game::Commodity::MEAT, { "meat", 5.0, 0.05, 0.05, 0.15}},
-		{game::Commodity::JOOLS, { "jools", 100.0, 0.05, 0.05, 0.15}},
-		{game::Commodity::SILK, { "silk", 50.0, 0.05, 0.05, 0.15}},
-		{game::Commodity::SEAWEED, { "seaweed", 0.01, 0.00, 0.00, 0.15}},
-		{game::Commodity::SALT, { "salt", 1.0, 0.02, 0.02, 0.15}}
+		{
+			game::Commodity::WHEAT, 
+			{ 
+				"wheat", 
+				1.0, 
+				0.05, 
+				0.05, 
+				0.15
+			}
+		},
+		{
+			game::Commodity::WOOD, 
+			{ 
+				"wood", 
+				7.5, 
+				0.05, 
+				0.05, 
+				0.15
+			}
+		},
+		{
+			game::Commodity::WOOL, 
+			{ 
+				"wool", 
+				2.5, 
+				0.05, 
+				0.05, 
+				0.15
+			}
+		},
+		{
+			game::Commodity::LABOR, 
+			{ 
+				"labor", 
+				1.0, 
+				0.05, 
+				0.05, 
+				0.15
+			}
+		},
+		{
+			game::Commodity::MEAT, 
+			{
+				"meat", 
+				5.0, 
+				0.05, 
+				0.05, 
+				0.15
+			}
+		},
+		{
+			game::Commodity::JOOLS, 
+			{ 
+				"jools", 
+				100.0, 
+				0.05, 
+				0.05, 
+				0.15
+			}
+		},
+		{
+			game::Commodity::SILK, 
+			{ 
+				"silk", 
+				50.0, 
+				0.05, 
+				0.05, 
+				0.15
+			}
+		},
+		{
+			game::Commodity::SEAWEED, 
+			{ 
+				"seaweed", 
+				0.01, 
+				0.00, 
+				0.00, 
+				0.15
+			}
+		},
+		{
+			game::Commodity::SALT, 
+			{ 
+				"salt", 
+				1.0, 
+				0.02, 
+				0.02, 
+				0.15
+			}
+		}
 	};
 
 	static std::list<game::Commodity> commodityList;
@@ -40,21 +121,21 @@ namespace game
 
 	double Commodities::GetBasePrice(const game::Commodity& commodity)
 	{
-		return commodityTable.find(commodity)->second.basePrice;
+		return Read(commodity).basePrice;
 	}
 
 	double Commodities::GetDemandFactor(const game::Commodity& commodity)
 	{
-		return commodityTable.find(commodity)->second.demandFactor;
+		return Read(commodity).demandFactor;
 	}
 
 	double Commodities::GetSupplyFactor(const game::Commodity& commodity)
 	{
-		return commodityTable.find(commodity)->second.supplyFactor;
+		return Read(commodity).supplyFactor;
 	}
 
 	double Commodities::GetDiscount(const game::Commodity& commodity)
 	{
-		return commodityTable.find(commodity)->second.discount;
+		return Read(commodity).discount;
 	}
 }
