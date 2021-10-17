61f518b96ec85d2bb1480187a05b4e7275b303bc
ernestpazera <ernestpazera@gmail.com>
2021-10-17 16:57:20 -0500

    dockresult

---
+++ seafarers_game_avatar/Game.Avatar.DockResult.h
@@ -0,0 +1,10 @@
+#pragma once
+namespace game::avatar//20211017
+{
+	enum class DockResult
+	{
+		DOCKED,
+		COMPLETED_QUEST,
+		ALREADY_DOCKED
+	};
+}
--- seafarers_game_avatar/Game.Avatar.Docked.h
+++ seafarers_game_avatar/Game.Avatar.Docked.h
@@ -1,17 +1,12 @@
 #pragma once
 #include <Common.XY.h>
 #include "Game.Avatar.Action.h"
+#include "Game.Avatar.DockResult.h"
 #include "Game.Avatar.State.h"
 #include "Game.Difficulty.h"
 #include <optional>
-namespace game::avatar
+namespace game::avatar//20211017
 {
-	enum class DockResult
-	{
-		DOCKED,
-		COMPLETED_QUEST,
-		ALREADY_DOCKED
-	};
 	struct Docked
 	{
 		static void Reset(const game::Difficulty&);
