787113b9e802616c5f71e87a20c77fc1c63560a7
ernestpazera <ernestpazera@gmail.com>
2021-10-19 05:21:41 -0500

    current job refactored

--- seafarers_states/State.InPlay.CurrentJob.cpp
+++ seafarers_states/State.InPlay.CurrentJob.cpp
@@ -21,6 +21,7 @@
 #include <Visuals.Texts.h>
 namespace state::in_play
 {
+	static const ::UIState CURRENT_STATE = ::UIState::IN_PLAY_CURRENT_JOB;
 	static const std::string LAYOUT_NAME = "State.InPlay.AvatarJob";
 	static const std::string MENU_ID = "CurrentJob";
 	static const std::string MENU_ITEM_ABANDON_JOB = "Abandon";
@@ -54,17 +55,18 @@ namespace state::in_play
 		{::Command::RED, ::application::UIState::GoTo(::UIState::IN_PLAY_NEXT) }
 	};
 
-	static void UpdateQuestText(const game::Quest& questModel)//TODO: duplicated!
+	static void UpdateQuestText(const game::Quest& questModel)
 	{
-		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE1, "Please deliver this");//TODO: hardcoded
+		auto islandModel = game::Islands::Read(questModel.destination).value();
+		double distance = common::Heading::Distance(questModel.destination, game::Ship::GetLocation());
+
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE1, "Please deliver this");
 		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE2, questModel.itemName);
 		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE3, "to {}", questModel.personName);
 		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE4, "the {}", questModel.professionName);
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE5, "at {} ({:.2f}).", islandModel.name, distance);
 		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE6, "Reward: {:.2f}", questModel.reward);
 
-		auto islandModel = game::Islands::Read(questModel.destination).value();
-		double distance = common::Heading::Distance(questModel.destination, game::Ship::GetLocation());
-		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE5, "at {} ({:.2f}).", islandModel.name, distance);
 		visuals::MenuItems::SetEnabled(LAYOUT_NAME, MENU_ITEM_ABANDON_JOB, true);
 	}
 
@@ -76,18 +78,18 @@ namespace state::in_play
 		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE4, "");
 		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE5, "");
 		visuals::Texts::SetText(LAYOUT_NAME, TEXT_LINE6, "");
+
 		visuals::MenuItems::SetEnabled(LAYOUT_NAME, MENU_ITEM_ABANDON_JOB, false);
 	}
 
 	static void UpdateText()
 	{
+		UpdateNoQuestText();
 		auto quest = game::avatar::Quest::Read();
 		if (quest)
 		{
 			UpdateQuestText(quest.value());
-			return;
 		}
-		UpdateNoQuestText();
 	}
 
 	static void OnEnter()
@@ -98,10 +100,22 @@ namespace state::in_play
 
 	void CurrentJob::Start()
 	{
-		::application::OnEnter::AddHandler(::UIState::IN_PLAY_CURRENT_JOB, OnEnter);
-		::application::MouseMotion::AddHandler(::UIState::IN_PLAY_CURRENT_JOB, visuals::Areas::HandleMenuMouseMotion(LAYOUT_NAME));
-		::application::MouseButtonUp::AddHandler(::UIState::IN_PLAY_CURRENT_JOB, visuals::Areas::HandleMenuMouseButtonUp(LAYOUT_NAME, ActivateItem));
-		::application::Command::SetHandlers(::UIState::IN_PLAY_CURRENT_JOB, commandHandlers);
-		::application::Renderer::SetRenderLayout(::UIState::IN_PLAY_CURRENT_JOB, LAYOUT_NAME);
+		::application::OnEnter::AddHandler(
+			CURRENT_STATE,
+			OnEnter);
+		::application::MouseMotion::AddHandler(
+			CURRENT_STATE,
+			visuals::Areas::HandleMenuMouseMotion(LAYOUT_NAME));
+		::application::MouseButtonUp::AddHandler(
+			CURRENT_STATE,
+			visuals::Areas::HandleMenuMouseButtonUp(
+				LAYOUT_NAME, 
+				ActivateItem));
+		::application::Command::SetHandlers(
+			CURRENT_STATE,
+			commandHandlers);
+		::application::Renderer::SetRenderLayout(
+			CURRENT_STATE,
+			LAYOUT_NAME);
 	}
 }
