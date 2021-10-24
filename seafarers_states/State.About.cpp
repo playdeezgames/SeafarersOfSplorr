3adb5740a232f194a85fb3dad1e6f48a9bc0b5fd
ernestpazera <ernestpazera@gmail.com>
2021-10-24 04:47:05 -0500

    i have made the about screen text based

--- seafarers_states/State.About.cpp
+++ seafarers_states/State.About.cpp
@@ -1,5 +1,6 @@
 #include <Application.Renderer.h>
 #include <Application.Command.h>
+#include <Application.Keyboard.h>
 #include <Application.MouseButtonUp.h>
 #include <Application.OnEnter.h>
 #include <Application.UIState.h>
@@ -11,17 +12,9 @@
 #include <Visuals.Terminals.h>
 namespace state
 {
-	static const std::string LAYOUT_NAME = "State.About";
+	static const std::string LAYOUT_NAME = "State.Terminal";
 	static const ::UIState CURRENT_STATE = ::UIState::ABOUT;
 	static const std::string TERMINAL_ID = "terminal";
-
-	static bool OnMouseButtonUp(const common::XY<int>& xy, MouseButton)
-	{
-		::application::UIState::Write(::UIState::MAIN_MENU);
-		return true;
-	}
-
-	static int counter = 0;
 
 	static void OnEnter()
 	{
@@ -32,13 +25,33 @@ namespace state
 		visuals::Terminals::WriteLine(LAYOUT_NAME, TERMINAL_ID, "A presentation of TheGrumpyGameDev");
 		visuals::Terminals::WriteLine(LAYOUT_NAME, TERMINAL_ID, "");
 		visuals::Terminals::WriteLine(LAYOUT_NAME, TERMINAL_ID, "0) Exit");
+		visuals::Terminals::WriteLine(LAYOUT_NAME, TERMINAL_ID, "");
+		visuals::Terminals::WriteText(LAYOUT_NAME, TERMINAL_ID, ">");
+	}
+
+	static const std::string KEY_RETURN = "Return";
+	static const std::string KEY_0 = "0";
+
+	static bool OnKeyDown(const std::string& key)
+	{
+		if (key == KEY_RETURN)
+		{
+			application::UIState::Write(::UIState::MAIN_MENU);
+			visuals::Terminals::WriteLine(LAYOUT_NAME, TERMINAL_ID, "");
+			return true;
+		}
+		else if (key == KEY_0)
+		{
+			visuals::Terminals::WriteText(LAYOUT_NAME, TERMINAL_ID, key);
+			return true;
+		}
+		return false;
 	}
 
 	void About::Start()
 	{
 		::application::OnEnter::AddHandler(CURRENT_STATE, OnEnter);
-		::application::MouseButtonUp::AddHandler(CURRENT_STATE, OnMouseButtonUp);
-		::application::Command::SetHandler(CURRENT_STATE, ::application::UIState::GoTo(::UIState::MAIN_MENU));
 		::application::Renderer::SetRenderLayout(CURRENT_STATE, LAYOUT_NAME);
+		::application::Keyboard::AddHandler(CURRENT_STATE, OnKeyDown);
 	}
 }
