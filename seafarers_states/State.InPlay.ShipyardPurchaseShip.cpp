609a42c89e52fdd415beffc01bf672fd48e78ba0
ernestpazera <ernestpazera@gmail.com>
2021-11-05 04:26:48 -0500

    i can buy a ship from a text menu!

--- seafarers_states/State.InPlay.ShipyardPurchaseShip.cpp
+++ seafarers_states/State.InPlay.ShipyardPurchaseShip.cpp
@@ -2,14 +2,56 @@
 #include <Application.OnEnter.h>
 #include <Application.Renderer.h>
 #include <Application.UIState.h>
+#include <Common.Data.h>
+#include <Common.Utility.Table.h>
 #include <Game.Audio.Mux.h>
+#include <Game.Avatar.Docked.h>
+#include <Game.Avatar.Items.h>
+#include <Game.Avatar.Ship.h>
+#include <Game.Avatar.Statistics.h>
 #include <Game.Colors.h>
+#include <Game.Islands.Markets.h>
+#include <Game.Islands.Ships.h>
+#include <Game.Player.h>
+#include <Game.Ship.h>
+#include <Game.ShipNames.h>
+#include <Game.ShipTypes.h>
 #include "State.InPlay.ShipyardPurchaseShip.h"
 #include "State.Terminal.h"
 #include "UIState.h"
+#include <Visuals.Confirmations.h>
+#include <Visuals.Messages.h>
 namespace state::in_play
 {
 	static const ::UIState CURRENT_STATE = ::UIState::IN_PLAY_SHIPYARD_PURCHASE_SHIP;
+
+	static std::map<game::ShipType, double> shipPrices;
+
+	static void UpdateShipPrices()
+	{
+		auto location = game::avatar::Docked::ReadLocation().value();
+		auto prices = game::islands::Ships::GetPurchasePrices(location);
+		auto tradeIn = game::islands::Ships::GetSalePrice(location, game::Ship::GetShipType(game::avatar::Ship::Read().value().shipId).value());
+		shipPrices.clear();
+		for (auto price : prices)
+		{
+			shipPrices[price.first] = price.second - tradeIn;
+		}
+	}
+
+	static void RefreshShipPrices()
+	{
+		size_t index = 1;
+		Terminal::SetForeground(game::Colors::YELLOW);
+		for (auto shipPrice : shipPrices)
+		{
+			Terminal::WriteLine(
+				"{}) {} (net cost {:.4f})", 
+				index++, 
+				game::ShipTypes::GetName(shipPrice.first), 
+				shipPrice.second);
+		}
+	}
 
 	static void Refresh()
 	{
@@ -17,7 +59,13 @@ namespace state::in_play
 
 		Terminal::SetForeground(game::Colors::LIGHT_CYAN);
 		Terminal::WriteLine("Purchase Ship:");
-		Terminal::WriteLine();
+		int shipId = game::avatar::Ship::ReadShipId().value();
+		auto shipType = game::Ship::GetShipType(shipId).value();
+		Terminal::SetForeground(game::Colors::GRAY);
+		Terminal::WriteLine("You currently have a {}.", game::ShipTypes::GetName(shipType));
+		Terminal::WriteLine("You have {:.4f}.", game::avatar::Statistics::ReadMoney(game::Player::GetAvatarId()));
+		Terminal::WriteLine("Prices shown after trade-in value.");
+		RefreshShipPrices();
 
 		Terminal::SetForeground(game::Colors::YELLOW);
 		Terminal::WriteLine("0) Never mind");
@@ -29,6 +77,7 @@ namespace state::in_play
 	static void OnEnter()
 	{
 		game::audio::Mux::Play(game::audio::Theme::MAIN);
+		UpdateShipPrices();
 		Refresh();
 	}
 
@@ -42,6 +91,72 @@ namespace state::in_play
 		{ "0", OnLeave }
 	};
 
+	static void BuyShip(game::ShipType desiredShipType, double price)
+	{
+		auto location = game::avatar::Docked::ReadLocation().value();
+		auto currentShipId = game::avatar::Ship::Read().value().shipId;
+		auto currentShipType = game::Ship::GetShipType(currentShipId).value();
+		game::avatar::Statistics::ChangeMoney(game::Player::GetAvatarId(), -price);
+		auto desiredShipId = game::Ship::Add({ desiredShipType,game::ShipNames::Generate(), location, 0.0, 1.0 });
+		//TODO: transfer crew/passengers/captives?
+		game::avatar::Ship::Write({ desiredShipId, game::BerthType::CAPTAIN });
+		game::islands::Markets::BuyShipType(location, desiredShipType);
+		game::islands::Markets::SellShipType(location, currentShipType);
+		UpdateShipPrices();
+		Refresh();
+	}
+
+	static void CheckTonnage(game::ShipType desiredShip, double price)
+	{
+		if (game::avatar::Items::TotalTonnage(game::Player::GetAvatarId()) <= game::ShipTypes::GetTotalTonnage(desiredShip))
+		{
+			Terminal::WriteLine();
+			Terminal::SetForeground(game::Colors::GREEN);
+			Terminal::WriteLine("You buy a new ship!");
+			BuyShip(desiredShip, price);
+			Refresh();
+		}
+		else
+		{
+			Terminal::ErrorMessage("Too Much Cargo!");
+			Refresh();
+		}
+	}
+
+	static void CheckAvailableFunds(game::ShipType desiredShip)
+	{
+		double price = shipPrices[desiredShip];
+		if (game::avatar::Statistics::ReadMoney(game::Player::GetAvatarId()) >= price)
+		{
+			CheckTonnage(desiredShip, price);
+		}
+		else
+		{
+			Terminal::ErrorMessage("Insufficient Funds!");
+			Refresh();
+		}
+	}
+
+	static void TryBuyShip(int hiliteRow)
+	{
+		game::ShipType desiredShip = common::utility::Table::GetNthKey(shipPrices, hiliteRow).value();
+		CheckAvailableFunds(desiredShip);
+	}
+
+	static void OnOtherInput(const std::string& line)
+	{
+		int index = common::Data::ToInt(line) - 1;
+		if (index >= 0 && index < shipPrices.size())
+		{
+			TryBuyShip(index);
+		}
+		else
+		{
+			Terminal::ErrorMessage(Terminal::INVALID_INPUT);
+			Refresh();
+		}
+	}
+
 	void ShipyardPurchaseShip::Start()
 	{
 		::application::OnEnter::AddHandler(CURRENT_STATE, OnEnter);
@@ -49,9 +164,6 @@ namespace state::in_play
 		::application::Keyboard::AddHandler(
 			CURRENT_STATE,
 			Terminal::DoIntegerInput(
-				menuActions,
-				Terminal::INVALID_INPUT,
-				Refresh));
-
+				menuActions,OnOtherInput));
 	}
 }
