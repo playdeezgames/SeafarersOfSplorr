6eb42a83835582b2c2a0bb05b6fd54d959592e3e
ernestpazera <ernestpazera@gmail.com>
2021-11-29 05:36:30 -0600

    game equipslots

--- seafarers_game/Game.EquipSlots.cpp
+++ seafarers_game/Game.EquipSlots.cpp
@@ -1,8 +1,7 @@
-#include <Common.Utility.h>
 #include <Common.Utility.List.h>
 #include "Game.EquipSlots.h"
 #include <map>
-namespace game//20211015
+namespace game
 {
 	struct EquipSlotDescriptor
 	{
