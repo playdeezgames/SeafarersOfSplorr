8bf95ae954790c304375322e14dd90471d8f7028
ernestpazera <ernestpazera@gmail.com>
2021-12-04 05:26:09 -0600

    added game layer ability to change counters for characters.

--- seafarers_game/Game.Characters.Counters.cpp
+++ seafarers_game/Game.Characters.Counters.cpp
@@ -1,3 +1,4 @@
+#include <Data.Game.Character.Counter.h>
 #include "Game.Characters.Counters.h"
 #include <map>
 namespace game::characters
@@ -11,5 +12,15 @@ namespace game::characters
 	const std::string& Counters::GetName(const Counter& counter)
 	{
 		return counterNames.find(counter)->second;
+	}
+
+	void Counters::Write(int characterId, const Counter& counter, int value)
+	{
+		data::game::character::Counter::Write(characterId, (int)counter, value);
+	}
+
+	std::optional<int> Counters::Read(int characterId, const Counter& counter)
+	{
+		return data::game::character::Counter::Read(characterId, (int)counter);
 	}
 }
--- seafarers_game/Game.Characters.Counters.h
+++ seafarers_game/Game.Characters.Counters.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "Game.Characters.Counter.h"
+#include <optional>
 #include <string>
 namespace game::characters
 {
 	struct Counters
 	{
 		static const std::string& GetName(const Counter&);
+		static void Write(int, const Counter&, int);
+		static std::optional<int> Read(int, const Counter&);
 	};
 }
