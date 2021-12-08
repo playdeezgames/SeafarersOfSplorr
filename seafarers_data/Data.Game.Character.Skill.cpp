ace353f85d5e49af83619a6dbc0a3a3aa3e27c25
ernestpazera <ernestpazera@gmail.com>
2021-12-08 12:02:55 -0600

    the data layer for character skills

---
+++ seafarers_data/Data.Game.Character.Skill.cpp
@@ -0,0 +1,43 @@
+#include <Common.Data.h>
+#include "Data.Game.Character.Skill.h"
+#include "Data.Game.Common.h"
+namespace data::game::character
+{
+	static const std::string CREATE_TABLE = "CREATE TABLE IF NOT EXISTS [CharacterSkills]([CharacterId] INT NOT NULL,[SkillId] INT NOT NULL,[Value] INT NOT NULL, UNIQUE([CharacterId],[SkillId]));";
+	static const std::string REPLACE_ITEM = "REPLACE INTO [CharacterSkills]([CharacterId],[SkillId],[Value]) VALUES({},{},{});";
+	static const std::string QUERY_ITEM = "SELECT [Value] FROM [CharacterSkills] WHERE [CharacterId]={} AND [SkillId]={};";
+	static const std::string QUERY_ITEMS = "SELECT [SkillId],[Value] FROM [CharacterSkills] WHERE [CharacterId]={};";
+	static const std::string FIELD_VALUE = "Value";
+	static const std::string FIELD_SKILL_ID = "SkillId";
+	static const auto AutoCreateTable = data::game::Common::Run(CREATE_TABLE);
+
+	void Skill::Write(int characterId, int skillId, int value)
+	{
+		AutoCreateTable();
+		Common::Execute(REPLACE_ITEM, characterId, skillId, value);
+	}
+
+	std::optional<int> Skill::Read(int characterId, int skillId)
+	{
+		AutoCreateTable();
+		auto records = Common::Execute(QUERY_ITEM, characterId, skillId);
+		if (!records.empty())
+		{
+			return common::Data::ToOptionalInt(records.front()[FIELD_VALUE]);
+		}
+		return std::nullopt;
+	}
+
+	std::map<int, int> Skill::Read(int characterId)
+	{
+		AutoCreateTable();
+		std::map<int, int> result;
+		auto records = Common::Execute(QUERY_ITEMS, characterId);
+		for (auto record : records)
+		{
+			result[common::Data::ToInt(record[FIELD_SKILL_ID])] = 
+				common::Data::ToInt(record[FIELD_VALUE]);
+		}
+		return result;
+	}
+}
---
+++ seafarers_data/Data.Game.Character.Skill.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <map>
+#include <optional>
+namespace data::game::character
+{
+	struct Skill
+	{
+		static void Write(int, int, int);
+		static std::optional<int> Read(int, int);
+		static std::map<int, int> Read(int);
+	};
+}
