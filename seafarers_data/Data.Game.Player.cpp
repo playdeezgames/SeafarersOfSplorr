454b75aae47b17425aa26f8ba8e6e337216e9d7a
ernestpazera <ernestpazera@gmail.com>
2021-12-23 10:45:32 -0600

    player

--- seafarers_data/Data.Game.Player.cpp
+++ seafarers_data/Data.Game.Player.cpp
@@ -3,25 +3,26 @@
 #include "Data.Game.Player.h"
 namespace data::game
 {
+	using namespace std::string_literals;
 	static const std::string CREATE_TABLE = 
-		"CREATE TABLE IF NOT EXISTS [Players]"
-		"("
-			"[PlayerId] INT NOT NULL UNIQUE CHECK([PlayerId]=1),"
-			"[CharacterId] INT NOT NULL UNIQUE"
-		");";
+		R"(CREATE TABLE IF NOT EXISTS [Players]
+		(
+			[PlayerId] INT NOT NULL UNIQUE CHECK([PlayerId]=1),
+			[CharacterId] INT NOT NULL UNIQUE
+		);)"s;
 	static const std::string QUERY_ITEM = 
-		"SELECT "
-			"[CharacterId] "
-		"FROM [Players] "
-		"WHERE "
-			"[PlayerId]={};";
+		R"(SELECT 
+			[CharacterId] 
+		FROM [Players] 
+		WHERE 
+			[PlayerId]={};)"s;
 	static const std::string REPLACE_ITEM = 
-		"REPLACE INTO [Players]"
-		"("
-			"[PlayerId],"
-			"[CharacterId]"
-		") "
-		"VALUES ({},{});";
+		R"(REPLACE INTO [Players]
+		(
+			[PlayerId],
+			[CharacterId]
+		) 
+		VALUES ({},{});)"s;
 
 	static const std::string FIELD_AVATAR_ID = "CharacterId";
 
