f2676cd350ee1669943fbe03ffc78bf599936382
ernestpazera <ernestpazera@gmail.com>
2022-01-03 05:50:48 -0600

    change heading now uses terminal menu

--- seafarers_states/State.InPlay.ChangeHeading.cpp
+++ seafarers_states/State.InPlay.ChangeHeading.cpp
@@ -23,70 +23,38 @@ namespace state::in_play
 			.GetShip()
 			.GetHeading());
 
-		Terminal::SetForeground(game::Colors::YELLOW);
-		if (playerCharacter
-			.GetKnownIslands().HasAny())
-		{
-			Terminal::WriteLine("1) Head for a known island");
-		}
-		if (playerCharacter
-			.GetBerth()
-			.GetShip()
-			.GetNearbyIslands().HasAny())
-		{
-			Terminal::WriteLine("2) Head for a nearby island");
-		}
-		Terminal::WriteLine("4) Set heading manually");
-		Terminal::WriteLine("0) Never mind");
+		Terminal::ShowMenu();
 
 		Terminal::ShowPrompt();
+	}
+
+	static void UpdateMenu()
+	{
+		auto playerCharacter =
+			game::Session()
+			.GetPlayer().GetCharacter();
+		auto ship = playerCharacter.GetBerth().GetShip();
+		Terminal::menu.Clear();
+		Terminal::menu.SetRefresh(Refresh);
+		if (playerCharacter.GetKnownIslands().HasAny())
+		{
+			Terminal::menu.AddAction({ "Head for a known island", application::UIState::GoTo(::UIState::IN_PLAY_HEAD_FOR_KNOWN) });
+		}
+		if (ship.GetNearbyIslands().HasAny())
+		{
+			Terminal::menu.AddAction({ "Head for a nearby island", application::UIState::GoTo(::UIState::IN_PLAY_HEAD_FOR_NEAR_BY) });
+		}
+		Terminal::menu.AddAction({ "Set heading manually", application::UIState::GoTo(::UIState::IN_PLAY_MANUAL_HEADING) });
+		MenuAction defaultAction = { "Never mind", application::UIState::GoTo(::UIState::IN_PLAY_SHIP_STATUS) };
+		Terminal::menu.SetDefaultAction(defaultAction);
 	}
 
 	static void OnEnter()
 	{
 		PlayMainTheme();
+		UpdateMenu();
 		Refresh();
 	}
-
-	static void OnHeadForKnownIsland()
-	{
-		if (game::Session()
-			.GetPlayer().GetCharacter()
-			.GetKnownIslands().HasAny())
-		{
-			application::UIState::Write(::UIState::IN_PLAY_HEAD_FOR_KNOWN);
-		}
-		else
-		{
-			Terminal::ErrorMessage(Terminal::INVALID_INPUT);
-			Refresh();
-		}
-	}
-
-	static void OnHeadForNearbyIsland()
-	{
-		if (game::Session()
-			.GetPlayer().GetCharacter()
-			.GetBerth()
-			.GetShip()
-			.GetNearbyIslands().HasAny())
-		{
-			application::UIState::Write(::UIState::IN_PLAY_HEAD_FOR_NEAR_BY);
-		}
-		else
-		{
-			Terminal::ErrorMessage(Terminal::INVALID_INPUT);
-			Refresh();
-		}
-	}
-
-	static const std::map<std::string, std::function<void()>> menuActions =
-	{
-		{"1", OnHeadForKnownIsland},
-		{"2", OnHeadForNearbyIsland},
-		{"4", application::UIState::GoTo(::UIState::IN_PLAY_MANUAL_HEADING)},
-		{"0", application::UIState::GoTo(::UIState::IN_PLAY_SHIP_STATUS)}
-	};
 
 	void ChangeHeading::Start()
 	{
@@ -94,8 +62,7 @@ namespace state::in_play
 		::application::Renderer::SetRenderLayout(CURRENT_STATE, Terminal::LAYOUT_NAME);
 		::application::Keyboard::AddHandler(
 			CURRENT_STATE,
-			Terminal::DoIntegerInput(
-				menuActions,
+			Terminal::DoMenuInput(
 				Terminal::INVALID_INPUT,
 				Refresh));
 	}
