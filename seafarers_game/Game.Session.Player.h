62bd10a526aeb957efca6d300c322ec7470f8c47
ernestpazera <ernestpazera@gmail.com>
2022-01-09 05:58:31 -0600

    notes for moving stuff from game session player.

--- seafarers_game/Game.Session.Player.h
+++ seafarers_game/Game.Session.Player.h
@@ -12,34 +12,44 @@ namespace game::session
 {
 	struct Player
 	{
-		static const size_t SKILL_CATEGORY_COUNT = 10;
-		static const size_t PROFESSIONAL_SKILL_POINT_COUNT = 300;
+		constexpr Player() {}
 		void Reset() const;
 		void Populate(const Difficulty& difficulty) const;
 		Character GetCharacter() const;
 		std::optional<Character> TryGetCharacter() const;
-		//coordinates UI metastate
+
+		//TODO: these dont make sense here - they goes somewhere in states
 		static void SetCrewDetailCharacterId(int characterId);
 		static int GetCrewDetailCharacterId();
+
 		static void SetRenameShipAdjective(const std::string& adjective);
 		static const std::string& GetRenameShipAdjective();
+
 		static void SetHeadForMark(const std::string& markName);
 		static const std::string GetHeadForMark();
+
 		static void SetProfession(const game::Profession& profession);
 		static game::Profession GetProfession();
+
+		static const size_t SKILL_CATEGORY_COUNT = 10;
 		static void ClearElectiveSkillCategories();
 		static std::set<game::session::SkillCategory> GetElectiveSkillCategories();
 		static void AddElectiveSkillCategory(const game::SkillCategory& category);
+		static const std::set<Skill>& GetProfessionalSkillSet();
+
 		static void SetCharacterCreationDefaults();
+
+		static const size_t PROFESSIONAL_SKILL_POINT_COUNT = 300;
 		static void ClearProfessionSkillPointAllocations();
 		static const std::map<int, size_t>& GetProfessionSkillPointAllocations();
 		static size_t GetProfessionalSkillPointsAllocated();
 		static size_t GetProfessionalSkillPointsRemaining();
 		static void AllocateProfessionalSkillPoints(int skillId, size_t points);
 		static void GenerateProfessionalSkillList();
-		static const std::set<Skill>& GetProfessionalSkillSet();
+
 		static void SetSelectedSkillId(int skillId);
 		static int GetSelectedSkillId();
+
 		static void ClearPersonalSkillAllocations();
 		static size_t GetPersonalSkillPointsAllocated();
 		static size_t GetPersonalSkillPointCount();
