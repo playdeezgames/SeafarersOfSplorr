7058015c1859198cd6dc0c17b856e7314ca1fde8
ernestpazera <ernestpazera@gmail.com>
2022-01-10 16:05:13 -0600

    a note

--- seafarers_game/Game.Session.World.cpp
+++ seafarers_game/Game.Session.World.cpp
@@ -5,7 +5,7 @@
 #include "Game.Session.World.h"
 namespace game::session
 {
-	static item::Type CreateWorldCurrencyItemSubtype()
+	static item::Type CreateWorldCurrencyItemSubtype()//TODO: should this go to Items::Populate?
 	{
 		const double WORLD_CURRENCY_JOOLS_AMOUNT = 0.001;
 		const std::string WORLD_CURRENCY_JOOLS_NAME = "jools";
