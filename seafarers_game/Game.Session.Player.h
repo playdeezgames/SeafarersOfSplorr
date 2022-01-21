4392a7c458d3f4870d18269f0c257c50b8da5644
ernestpazera <ernestpazera@gmail.com>
2022-01-21 05:03:57 -0600

    and now we begin to reshape the world

--- seafarers_game/Game.Session.Player.h
+++ seafarers_game/Game.Session.Player.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "Game.Difficulty.h"
-#include "Game.SkillCategory.h"
 #include "Game.Profession.h"
 #include "Game.Session.Character.h"
 #include "Game.Session.Skill.h"
 #include "Game.Session.SkillCategory.h"
+#include "Game.SkillCategory.h"
 #include <map>
 #include <optional>
 #include <set>
--- seafarers_game/Game.Session.World.h
+++ seafarers_game/Game.Session.World.h
@@ -21,23 +21,23 @@ namespace game::session
 	{
 		constexpr explicit World() : worldId(WORLD_ID) {}
 
-		item::Type GetCurrencyItemSubtype() const;
-		game::Difficulty GetDifficulty() const;
-
 		constexpr world::Bounds GetBounds() const { return world::Bounds(worldId); }
 		constexpr world::Calendar GetCalendar() const { return world::Calendar(worldId); }
-		constexpr session::Demigods GetDemigods() const { return session::Demigods(); }
 		constexpr world::Distances GetDistances() const { return world::Distances(worldId); }
+		constexpr world::Wind GetWind() const { return world::Wind(worldId); }
+
+		constexpr session::Demigods GetDemigods() const { return session::Demigods(); }
 		constexpr Commodities GetCommodities() const { return Commodities(); }
 		constexpr world::Islands GetIslands() const { return world::Islands(); }
 		constexpr item::Types GetItemSubtypes() const { return item::Types(); }
-		constexpr world::Wind GetWind() const { return world::Wind(worldId); }
 		constexpr Tribes GetTribes() const { return Tribes(); }
 		constexpr world::ShipTypes GetShipTypes() const { return world::ShipTypes(); }
 		constexpr world::Skills GetSkills() const { return world::Skills(); }
 		constexpr world::Professions GetProfessions() const { return world::Professions(); }
 		constexpr SkillCategories GetSkillCategories() const { return SkillCategories(); }
 
+		item::Type GetCurrencyItemSubtype() const;
+		game::Difficulty GetDifficulty() const;
 		void Reset() const;
 		void Populate(const Difficulty& difficulty) const;
 		void ApplyTurnEffects() const;
