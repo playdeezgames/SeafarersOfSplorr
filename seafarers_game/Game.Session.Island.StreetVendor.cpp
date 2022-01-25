1c8a2ef3cf39717cb1d99f4de523c96fb31235ed
ernestpazera <ernestpazera@gmail.com>
2022-01-25 05:07:13 -0600

    things have names

--- seafarers_game/Game.Session.Island.StreetVendor.cpp
+++ seafarers_game/Game.Session.Island.StreetVendor.cpp
@@ -3,12 +3,45 @@
 #include <map>
 #include <Common.RNG.h>
 #include <Data.Game.StreetVendor.MenuItem.h>
+#include <format>
 namespace game::session::island
 {
 	static const std::map<size_t, size_t> foodMenuSizeGenerator =
 	{
 		{2, 2},
 		{3, 1}
+	};
+
+	using namespace std::string_view_literals;
+
+	static const std::vector<std::string_view> cookingStyles =
+	{
+		"pan-fried"sv,
+		"boiled"sv,
+		"baked"sv,
+		"roasted"sv,
+		"toasted"sv,
+		"deep-fried"sv,
+		"raw"sv,
+		"seared"sv,
+		"pickled"sv,
+		"salted"sv,
+		"dried"sv
+	};
+
+	static const std::vector<std::string_view> foodItems =
+	{
+		"potatos"sv,
+		"clams"sv,
+		"marmite"sv,
+		"toad stools"sv,
+		"rats"sv,
+		"coconuts"sv,
+		"kelp"sv,
+		"fish"sv,
+		"grubs"sv,
+		"pinapples"sv,
+		"lefsa"sv
 	};
 
 	void StreetVendor::Populate(const game::Difficulty&) const
@@ -19,7 +52,14 @@ namespace game::session::island
 		while (itemCount > 0)
 		{
 			auto cookingSkill = common::RNG::FromRange(80, 100);
-			data::game::street_vendor::MenuItem::Create(featureId, "BAD_NAME", cost, satiety, cookingSkill);
+			data::game::street_vendor::MenuItem::Create(
+				featureId, 
+				std::format("{} {}", 
+					common::RNG::FromVector(cookingStyles).value(), 
+					common::RNG::FromVector(foodItems).value()),
+				cost, 
+				satiety, 
+				cookingSkill);
 			cost += common::RNG::FromRange(1, 3);
 			satiety *= 2;
 			itemCount--;
