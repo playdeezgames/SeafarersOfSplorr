adfaf03f7606c2cab32bce035d0734db0d95beab
ernestpazera <ernestpazera@gmail.com>
2022-01-26 10:02:48 -0600

    inventory eating

--- seafarers_states/State.InPlay.CrewDetail.InventoryDetail.cpp
+++ seafarers_states/State.InPlay.CrewDetail.InventoryDetail.cpp
@@ -7,6 +7,7 @@
 #include "State.ScratchPad.SelectedItemType.h"
 #include <Game.Session.Character.ItemTypes.h>
 #include <Game.Session.Item.Type.h>
+#include <Game.Session.Character.Messages.h>
 namespace state::in_play::crew_detail
 {
 	std::optional<int> InventoryDetail::stateId = std::nullopt;
@@ -26,10 +27,30 @@ namespace state::in_play::crew_detail
 		Terminal::ShowPrompt();
 	}
 
+	static void EatItem()
+	{
+		if (game::session::Character(scratch_pad::CrewDetail::GetCharacterId())
+			.Eat(scratch_pad::SelectedItemType::GetItemTypeId()))
+		{
+			application::UIState::Write(Inventory::GetStateId());
+		}
+		else
+		{
+			game::session::character::Messages(scratch_pad::CrewDetail::GetCharacterId()).Add(game::Colors::RED, "You cannot eat that!");
+			Refresh();
+		}
+	}
+
 	static void UpdateMenu()
 	{
 		Terminal::menu.Clear();
 		Terminal::menu.SetRefresh(Refresh);
+		auto itemType =
+			game::session::item::Type(scratch_pad::SelectedItemType::GetItemTypeId());
+		if (itemType.HasCategory(game::item::Category::FOOD))
+		{
+			Terminal::menu.AddAction({ "Eat", EatItem });
+		}
 		MenuAction defaultAction = { "Never mind", application::UIState::DoGoTo(Inventory::GetStateId) };
 		Terminal::menu.SetDefaultAction(defaultAction);
 	}
